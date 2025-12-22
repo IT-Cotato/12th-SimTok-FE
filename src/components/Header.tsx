6992ced8e6f1c84d585a5a0cdf2eaf679cd8b0f9
idid10 <ejwuud2@naver.com>
2025-12-22 22:34:31 +0900

    style: Button 컴포넌트 분리

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,13 +15,10 @@ export default function Header({ title }: HeaderProps) {
   return (
     <header className="flex gap-[156px]">
       <button type="button" onClick={() => router.back()} className="h-6 w-6">
-        <BackIcon className="h-6 w-6" />
+        <BackIcon />
       </button>
 
       <span className="text-h3 whitespace-nowrap text-black">{title}</span>
-
-      {/* 오른쪽 자리를 맞추기 위한 더미 박스 */}
-      <div className="h-6 w-6" />
     </header>
   );
 }
