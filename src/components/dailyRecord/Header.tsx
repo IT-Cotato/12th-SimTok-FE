bd5b3484a78e9d7aae18e2b728072a4aec800bf4
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-12-29 11:44:21 +0900

    feat: 하루기록 페이지에 헤더, 네브바 컴포넌트 추가

--- src/components/dailyRecord/Header.tsx
+++ src/components/dailyRecord/Header.tsx
@@ -4,7 +4,7 @@ import PencilIcon from "@/assets/pencil.svg";
 export const Header = () => {
   return (
     <header className="flex items-center justify-end gap-[117px] px-4 py-[10px]">
-      <h1>하루기록</h1>
+      <h1>하루기록</h1> {/* 디자인추가해야함 */}
       <div className="flex items-center gap-2">
         <AlarmIcon className="h-6 w-6" />
         <PencilIcon className="h-6 w-6" />
