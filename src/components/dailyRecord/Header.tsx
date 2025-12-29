3ffafab33ce8745a2dbb4931347e51dac56f7664
lemoncurdyogurt <suhosinsu1@naver.com>
2025-12-29 20:01:26 +0900

    design: 하루기록 헤더 알람 여부에 따른 알람설정 추가 및 헤더 디자인 수정

--- src/components/dailyRecord/Header.tsx
+++ src/components/dailyRecord/Header.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
 import AlarmIcon from "@/assets/bell.svg";
 import PencilIcon from "@/assets/pencil.svg";
 
 export const Header = () => {
+  const [isAlaramNew, setIsAlarmNew] = useState(true);
+
   return (
-    <header className="flex items-center justify-end gap-[117px] px-4 py-[10px]">
-      <h1>하루기록</h1> {/* 디자인추가해야함 */}
-      <div className="flex items-center gap-2">
-        <AlarmIcon className="h-6 w-6" />
+    <header className="relative flex items-center justify-center px-4 py-[10px]">
+      <h1>하루기록</h1>
+      <div className="absolute right-4 flex gap-2">
+        {isAlaramNew ? (
+          <button
+            className="relative cursor-pointer"
+            onClick={() => setIsAlarmNew(false)}
+          >
+            <AlarmIcon className="h-6 w-6" />
+            <span className="bg-orange-00 absolute top-[3px] right-1 h-1 w-1 rounded-full" />
+          </button>
+        ) : (
+          <AlarmIcon className="h-6 w-6" />
+        )}
         <PencilIcon className="h-6 w-6" />
       </div>
     </header>
