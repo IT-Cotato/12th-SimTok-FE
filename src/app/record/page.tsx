4afd07a9e6c522c54a5227d368485f76b9e39d91
lemoncurdyogurt <suhosinsu1@naver.com>
2025-12-30 15:31:20 +0900

    feat: 하루한컷 미션 피드 추가

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,13 +1,16 @@
 import { NavBar } from "@/components/common/NavBar";
 import { Header } from "@/components/dailyRecord/Header";
 import { RecordMissionCard } from "@/components/dailyRecord/MissionCard";
+import { RecordMissionFeed } from "@/components/dailyRecord/MissionFeed";
 
 const DailyRecordPage = () => {
   return (
     <main className="flex w-full flex-col pb-[112px]">
       <Header />
       <RecordMissionCard />
-      <div className="mt-[13px]"></div>
+      <div className="mt-[13px]">
+        <RecordMissionFeed />
+      </div>
       <NavBar />
     </main>
   );
