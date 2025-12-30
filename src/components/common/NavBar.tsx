6f704f3f424cf5e636ca1c762da7b256c3921368
lemoncurdyogurt <suhosinsu1@naver.com>
2025-12-30 13:51:56 +0900

    design: 네비바 ui수정

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -9,7 +9,7 @@ export const NavBar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="grid grid-cols-1 items-center justify-center">
+    <nav className="fixed bottom-0 left-1/2 z-50 grid h-[112px] w-full max-w-[440px] -translate-x-1/2 grid-cols-5 bg-white">
       {NAV_ITEMS.map(({ key, label, href, icons }) => {
         const isActive =
           href === "/" ? pathname === "/" : pathname.startsWith(href);
@@ -18,11 +18,10 @@ export const NavBar = () => {
 
         return (
           <Link key={key} href={href}>
-            <div className="flex flex-col">
-              {" "}
+            <div className="flex flex-col items-center justify-center">
               {/* 디자인추가해야함 */}
-              <Icon />
-              <p>{label}</p>
+              <Icon className="h-6 w-6" />
+              <p className="text-neutral-04 text-navigation">{label}</p>
             </div>
           </Link>
         );
