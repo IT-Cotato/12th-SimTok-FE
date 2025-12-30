1ea8cde64bf4b1917ab9235635f3298bf45221f4
lemoncurdyogurt <suhosinsu1@naver.com>
2025-12-30 18:03:38 +0900

    design: 감정이모지 사이즈 조정 및 감정묘사 텍스트 수정

--- src/components/dailyRecord/ShareDiaryCard.tsx
+++ src/components/dailyRecord/ShareDiaryCard.tsx
@@ -11,26 +11,27 @@ export const SharedDiaryCard = () => {
         const emotionMeta = getEmotionMeta(item.emotion);
         return (
           <section key={item.id} className="flex flex-col">
-            <div className="flex justify-center gap-[5px] px-4 py-[10px]">
+            <div className="flex items-center justify-start gap-[5px] px-4 py-[10px]">
               <Image
                 src={item.profile}
                 alt={item.userName}
                 width={46}
                 height={46}
+                className="rounded-full object-cover"
               />
               <p>{item.userName}</p>
             </div>
             {emotionMeta && (
-              <h3 className="flex flex-col items-center gap-[-16px]">
+              <h3 className="-mt-[32px] flex flex-col items-center">
                 <Image
                   src={emotionMeta.imageSrc}
                   alt={item.emotion}
-                  width={69}
-                  height={69}
+                  width={89}
+                  height={89}
                   className="p-[10px]"
                 />
-                <p className="text-sub1-sb text-black">
-                  {emotionMeta.pastText}
+                <p className="text-sub1-sb -mt-[16px] text-black">
+                  {`"오늘 하루는 {emotionMeta.pastText}"`}
                 </p>
               </h3>
             )}
