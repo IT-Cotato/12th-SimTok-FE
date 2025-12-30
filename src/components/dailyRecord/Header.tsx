a2e1fddc99cdea5e1abf91d58f94799762b43568
lemoncurdyogurt <suhosinsu1@naver.com>
2025-12-30 13:52:25 +0900

    design: 아이콘에 커서포인터 추가

--- src/components/dailyRecord/Header.tsx
+++ src/components/dailyRecord/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
   const [isAlaramNew, setIsAlarmNew] = useState(true);
 
   return (
-    <header className="relative flex items-center justify-center px-4 py-[10px]">
+    <header className="relative mt-[13px] flex items-center justify-center px-4 py-[10px]">
       <h1>하루기록</h1>
       <div className="absolute right-4 flex gap-2">
         {isAlaramNew ? (
@@ -17,13 +17,13 @@ export const Header = () => {
             className="relative cursor-pointer"
             onClick={() => setIsAlarmNew(false)}
           >
-            <AlarmIcon className="h-6 w-6" />
+            <AlarmIcon className="h-6 w-6 cursor-pointer" />
             <span className="bg-orange-00 absolute top-[3px] right-1 h-1 w-1 rounded-full" />
           </button>
         ) : (
-          <AlarmIcon className="h-6 w-6" />
+          <AlarmIcon className="h-6 w-6 cursor-pointer" />
         )}
-        <PencilIcon className="h-6 w-6" />
+        <PencilIcon className="h-6 w-6 cursor-pointer" />
       </div>
     </header>
   );
