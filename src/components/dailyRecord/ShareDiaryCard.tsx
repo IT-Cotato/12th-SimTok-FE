5a2f8f23823309bceb6280592ea2144108e33fb8
lemoncurdyogurt <suhosinsu1@naver.com>
2025-12-30 17:51:11 +0900

    feat: 공유일기 카드 섹션 추가

--- src/components/dailyRecord/ShareDiaryCard.tsx
+++ src/components/dailyRecord/ShareDiaryCard.tsx
@@ -1,1 +1,42 @@
-export const SharedDiaryCard = () => {};
+import Image from "next/image";
+
+import ShareDiaryData from "@/mock/sharedDiary.json";
+
+import { getEmotionMeta } from "@/utils/getEmotionMeta";
+
+export const SharedDiaryCard = () => {
+  return (
+    <div>
+      {ShareDiaryData.map(item => {
+        const emotionMeta = getEmotionMeta(item.emotion);
+        return (
+          <section key={item.id} className="flex flex-col">
+            <div className="flex justify-center gap-[5px] px-4 py-[10px]">
+              <Image
+                src={item.profile}
+                alt={item.userName}
+                width={46}
+                height={46}
+              />
+              <p>{item.userName}</p>
+            </div>
+            {emotionMeta && (
+              <h3 className="flex flex-col items-center gap-[-16px]">
+                <Image
+                  src={emotionMeta.imageSrc}
+                  alt={item.emotion}
+                  width={69}
+                  height={69}
+                  className="p-[10px]"
+                />
+                <p className="text-sub1-sb text-black">
+                  {emotionMeta.pastText}
+                </p>
+              </h3>
+            )}
+          </section>
+        );
+      })}
+    </div>
+  );
+};
