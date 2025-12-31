0705076250f8ab0aa266fad3e141823a3af25baa
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-12-31 13:23:35 +0900

    feat: 공유일기 cta 메세지 추가

---
+++ src/components/dailyRecord/InfoMessage.tsx
@@ -0,0 +1,10 @@
+export const InfoMessage = () => {
+  return (
+    <section className="flex flex-col">
+      <div className="border-b-neutral-01 mx-[10px] h-0 w-0 self-end border-t-0 border-r-[9px] border-b-[18px] border-l-[9px] border-solid border-t-transparent border-r-transparent border-l-transparent"></div>
+      <div className="bg-neutral-01 text-sub2-sb border-neutral-01 -mt-[6px] rounded-[100px] border px-[10px] py-2 text-white">
+        공유일기를 작성하고 친구들과 공유해보세요!
+      </div>
+    </section>
+  );
+};
