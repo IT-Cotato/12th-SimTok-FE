e26c1c4063fa3517989503c3b6810e8310c318de
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-02 20:14:12 +0900

    feat: 댓글 아이콘 클릭 시 생기는 댓글창 추가

--- src/components/dailyRecord/SharedDiaryChat.tsx
+++ src/components/dailyRecord/SharedDiaryChat.tsx
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import CloseIcon from "@/assets/close.svg";
+
+import MyProfile from "@/mock/myProfile.json";
+
+import { MessageInput } from "../common/MessageInput";
+
 export const SharedDiaryChat = () => {
+  const profileImg = MyProfile.profileImg;
+  const router = useRouter();
+
   return (
-    <section className="fixed bottom-0 h-[545px] w-full max-w-[440px] bg-white">
-      <div></div>
-    </section>
+    <div className="fixed inset-0 bottom-0 left-1/2 z-50 flex w-full max-w-[440px] -translate-x-1/2 items-end justify-center bg-black/50">
+      <section className="flex h-[545px] w-full max-w-[440px] flex-1 flex-col rounded-t-2xl bg-white">
+        <div className="h-[433px]">
+          <div className="w-full px-4 py-[10px]">
+            <button className="h-[14px] w-[14px] cursor-pointer">
+              <CloseIcon onClick={() => router.back()} />
+            </button>
+          </div>
+          <h3 className="text-h3 text-neutral-01 border-neutral-10 flex items-center justify-center border-b p-[10px]">
+            댓글
+          </h3>
+          <div className="flex flex-col items-center justify-center pt-[109px]">
+            <p className="text-sub1-sb text-neutral-01">아직 댓글이 없습니다</p>
+            <p className="text-sub2-r text-neutral-06">댓글을 남겨보세요!</p>
+          </div>
+        </div>
+        <footer className="fixed bottom-0 flex w-full max-w-[440px] items-center gap-[13px] bg-white px-4 pt-4 pb-10 shadow-[0_-2px_10px_0px_rgba(0,0,0,0.10)]">
+          <Image
+            src={profileImg}
+            alt="myProfileImage"
+            width={53}
+            height={53}
+            className="rounded-full object-cover"
+          />
+          <MessageInput />
+        </footer>
+      </section>
+    </div>
   );
 };
