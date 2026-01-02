40b65c6da87217028c40bc2af4e2ca6613ed0e51
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-02 17:25:43 +0900

    feat: 하단바 추가

--- src/app/day-story/[id]/page.tsx
+++ src/app/day-story/[id]/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useParams } from "next/navigation";
 
 import { BackHeader } from "@/components/common/BackHeader";
+import { StoryBottomBar } from "@/components/dailyRecord/StoryBottomBar";
 
 import DailyRecordData from "@/mock/dailyRecord.json";
 
@@ -21,10 +22,12 @@ const DailyStory = () => {
   const timeAgo = getTimeAgo(story.createdAt);
 
   return (
-    <section className="w-full">
-      <BackHeader title={`${story.userName}의 하루`} timeAgo={timeAgo} />
+    <section className="relative flex h-screen w-full items-center">
+      <div className="fixed top-0 w-full max-w-[440px]">
+        <BackHeader title={`${story.userName}의 하루`} timeAgo={timeAgo} />
+      </div>
 
-      <div className="relative mt-4 h-[calc(100vh-64px)] w-full">
+      <div className="absolute top-[66px] bottom-[114px] w-full max-w-[440px]">
         <Image
           src={story.image}
           alt={`${story.userName}의 하루한컷`}
@@ -32,6 +35,9 @@ const DailyStory = () => {
           className="object-cover"
           priority
         />
+      </div>
+      <div className="fixed bottom-0 w-full max-w-[440px]">
+        <StoryBottomBar />
       </div>
     </section>
   );
