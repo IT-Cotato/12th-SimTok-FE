2ccff86bba2a3b91b7171daef655245eaf6b5ead
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-02 17:53:28 +0900

    design: 전송아이콘 회전 제거

--- src/assets/SendButton.tsx
+++ src/assets/SendButton.tsx
@@ -2,7 +2,7 @@ import SendIcon from "@/assets/messenger.svg";
 
 export const SendButton = () => {
   return (
-    <button className="- absolute top-1 right-4 mx-[2px] my-[3px] rotate-[-23deg]">
+    <button className="- absolute top-1 right-4 px-[2px] py-[3px]">
       <SendIcon className="text-neutral-04 h-[18px] w-[20px]" />
     </button>
   );
