a058e4595e4aa7eb91ff3f7d17e59b54ce252ffd
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-02 13:20:24 +0900

    feat: 미션완료시 미션카드 분기 추가

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -4,11 +4,16 @@ import { RecordMissionCard } from "@/components/dailyRecord/MissionCard";
 import { RecordMissionFeed } from "@/components/dailyRecord/MissionFeed";
 import { SharedDiaryCard } from "@/components/dailyRecord/SharedDiaryCard";
 
+import DailyRecordData from "@/mock/dailyRecord.json";
+
 const DailyRecordPage = () => {
+  const MY_ID = 101;
+  const hasMyRecord = DailyRecordData.some(item => item.userId === MY_ID);
+
   return (
     <main className="flex w-full flex-col pb-[112px]">
       <Header />
-      <RecordMissionCard />
+      <RecordMissionCard hasMyRecord={hasMyRecord} />
       <div className="mt-[17.5px]">
         <RecordMissionFeed />
       </div>
