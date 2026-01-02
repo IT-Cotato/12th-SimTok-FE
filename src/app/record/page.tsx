9632b7c0e8285c74d7ba6afb691ed384199979e5
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-02 13:33:30 +0900

    rename: 미션안내 cta 카드 파일 이름 수정

--- src/app/record/page.tsx
+++ src/app/record/page.tsx
@@ -1,7 +1,7 @@
 import { NavBar } from "@/components/common/NavBar";
 import { Header } from "@/components/dailyRecord/Header";
-import { RecordMissionCard } from "@/components/dailyRecord/MissionCard";
 import { RecordMissionFeed } from "@/components/dailyRecord/MissionFeed";
+import { RecordMissionCard } from "@/components/dailyRecord/MissionInfo";
 import { SharedDiaryCard } from "@/components/dailyRecord/SharedDiaryCard";
 
 import DailyRecordData from "@/mock/dailyRecord.json";
