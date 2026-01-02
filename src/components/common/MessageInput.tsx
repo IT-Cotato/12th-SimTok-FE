5a61d7e61d7152de0118442d06c9d52992839fa9
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-02 20:11:28 +0900

    refactor: input focus outline 제거

--- src/components/common/MessageInput.tsx
+++ src/components/common/MessageInput.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 import { SendButton } from "@/assets/SendButton";
 
 export const MessageInput = () => {
+  const [text, setText] = useState("");
+  const hasText = Boolean(text);
+
   return (
     <div className="relative flex-1">
-      <input className="border-neutral-07 bg-neutral-11 w-full rounded-2xl border border-solid px-4 py-1" />
-      <SendButton />
+      <input
+        className="border-neutral-07 bg-neutral-11 w-full rounded-2xl border border-solid px-4 py-1 focus:outline-none"
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <SendButton hasText={hasText} />
     </div>
   );
 };
