263598c1e17c6d4af64bc69c83df302073ac2a28
idid10 <ejwuud2@naver.com>
2026-01-06 21:49:44 +0900

    refactor: 비밀번호 valid 로직 변경사항 반영

--- src/hooks/useProfileImageUpload.ts
+++ src/hooks/useProfileImageUpload.ts
@@ -2,29 +2,41 @@
 
 import { useState } from "react";
 
+import { uploadImageFile } from "@/utils/upload";
+
 export const useProfileImageUpload = () => {
   const [profileImage, setProfileImage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
 
+  // const uploadImage = async (file: File) => {
+  //   setIsLoading(true);
+
+  //   await new Promise(res => setTimeout(res, 2000));
+
+  //   const imageUrl = URL.createObjectURL(file);
+  //   setProfileImage(imageUrl);
+
+  //   setIsLoading(false);
+  // };
   const uploadImage = async (file: File) => {
-    setIsLoading(true);
-
-    await new Promise(res => setTimeout(res, 2000));
-
-    const imageUrl = URL.createObjectURL(file);
-    setProfileImage(imageUrl);
-
-    setIsLoading(false);
+    try {
+      setIsLoading(true);
+      const { url } = await uploadImageFile(file);
+      setProfileImage(url);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const resetImage = () => {
     setProfileImage(null);
   };
 
-  return {
-    profileImage,
-    isLoading,
-    uploadImage,
-    resetImage,
-  };
+  // return {
+  //   profileImage,
+  //   isLoading,
+  //   uploadImage,
+  //   resetImage,
+  // };
+  return { profileImage, isLoading, uploadImage, resetImage } as const;
 };
