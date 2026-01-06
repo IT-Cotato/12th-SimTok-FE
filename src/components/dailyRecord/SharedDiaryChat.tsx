3ca0d749792ce2deee2c473689fe8ddbc503a972
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-06 18:17:44 +0900

    feat: 댓글리스트 영역 컴포넌트 분리 후, 댓글 데이터 가져오기 로직 추가

--- src/components/dailyRecord/SharedDiaryChat.tsx
+++ src/components/dailyRecord/SharedDiaryChat.tsx
@@ -2,14 +2,14 @@
 import Image from "next/image";
 import { useParams, useRouter } from "next/navigation";
 
-import { useState } from "react";
-
 import CloseIcon from "@/assets/close.svg";
 
 import { MessageInput } from "@/components/common/MessageInput";
 
 import CommentData from "@/mock/diaryComment.json";
 import MyProfile from "@/mock/myProfile.json";
+
+import { CommentList } from "./CommentList";
 
 interface SharedDiaryCommentProps {
   variant?: "modal" | "page";
@@ -24,7 +24,6 @@ export const SharedDiaryComment = ({
   const numericId = Number(id);
 
   const isPage = variant === "page";
-  const [hasComment, setHasComment] = useState(false);
 
   return (
     <div
@@ -33,11 +32,15 @@ export const SharedDiaryComment = ({
           ? "min-h-screen"
           : "fixed inset-0 bottom-0 left-1/2 z-50 -translate-x-1/2 bg-black/50"
       } flex w-full max-w-[440px] items-end justify-center`}
+      onClick={() => {
+        if (!isPage) router.back();
+      }}
     >
       <section
         className={`flex w-full max-w-[440px] flex-col bg-white ${
           isPage ? "h-screen rounded-none" : "h-[545px] rounded-t-2xl"
         }`}
+        onClick={e => e.stopPropagation()}
       >
         <div className="h-[433px]">
           <div className="w-full px-4 py-[10px]">
@@ -48,10 +51,7 @@ export const SharedDiaryComment = ({
           <h3 className="text-h3 text-neutral-01 border-neutral-10 flex items-center justify-center border-b p-[10px]">
             댓글
           </h3>
-          <div className="flex flex-col items-center justify-center pt-[109px]">
-            <p className="text-sub1-sb text-neutral-01">아직 댓글이 없습니다</p>
-            <p className="text-sub2-r text-neutral-06">댓글을 남겨보세요!</p>
-          </div>
+          <CommentList comments={CommentData} />
         </div>
         <footer className="fixed bottom-0 flex w-full max-w-[440px] items-center gap-[13px] bg-white px-4 pt-4 pb-10 shadow-[0_-2px_10px_0px_rgba(0,0,0,0.10)]">
           <Image
