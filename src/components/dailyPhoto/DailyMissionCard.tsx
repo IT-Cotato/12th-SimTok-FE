0494d6ce7120a4841999254bc799d8b0b99e9f07
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-07 04:46:44 +0900

    design: 미션텍스트와 버튼 간격 수정

--- src/components/dailyPhoto/DailyMissionCard.tsx
+++ src/components/dailyPhoto/DailyMissionCard.tsx
@@ -28,14 +28,16 @@ export const DailyMissionCard = () => {
       <div className="bg-neutral-11 h-28 w-28 rounded-full">
         {/* 아이콘 영역 */}
       </div>
-      <div className="text-sub-number p-[10px]">
-        오늘의 하늘을 공유해주세요.
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-sub-number p-[10px]">
+          오늘의 하늘을 공유해주세요.
+        </div>
+        <button
+          className={`${status == "NOT_STARTED" ? "bg-gradient-orange" : "bg-green-01"} text-button-sb h-[50px] w-[90px] cursor-pointer rounded-2xl text-white`}
+        >
+          {MISSION_STATUS[status].buttonText}
+        </button>
       </div>
-      <button
-        className={`${status == "NOT_STARTED" ? "bg-gradient-orange" : "bg-green-01"} text-button-sb h-[50px] w-[90px] cursor-pointer rounded-2xl text-white`}
-      >
-        {MISSION_STATUS[status].buttonText}
-      </button>
     </section>
   );
 };
