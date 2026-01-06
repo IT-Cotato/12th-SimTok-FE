4746ae381322a324bdcd9539310234ae72847feb
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-06 14:44:08 +0900

    fix: 타입선언 잘못 수정

--- src/types/myProfile.type.ts
+++ src/types/myProfile.type.ts
@@ -1,6 +1,6 @@
-export type myProfile = () => {
-  profileImg: "string";
-  userName: "string";
-  phoneNumber: "string"; //01012345678
-  birthDate: "string"; //0000.00.00
+export type MyProfile = () => {
+  profileImg: string;
+  userName: string;
+  phoneNumber: string; //01012345678
+  birthDate: string; //0000.00.00
 };
