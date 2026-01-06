2eeab8a133e803ca7decf6a15f9f6062788d6c5f
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-06 17:25:18 +0900

    design: 텍스트가 있는 경우 border색 변경

--- src/assets/SendButton.tsx
+++ src/assets/SendButton.tsx
@@ -7,7 +7,7 @@ export const SendButton = ({ hasText = false }: SendButtonProps) => {
   return (
     <button className="absolute top-1 right-4 rotate-[-23deg] px-[2px] py-[3px]">
       <SendIcon
-        className={`h-[18px] w-[20px] ${hasText ? "text-green-02 cursor-pointer" : "text-neutral-04"}`}
+        className={`text-neutral-04 h-[18px] w-[20px] ${hasText ? "cursor-pointer" : ""}`}
       />
     </button>
   );
