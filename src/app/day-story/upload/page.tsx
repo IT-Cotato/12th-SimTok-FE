272b1a27f77f12118e59acee8a5e9aa41997f079
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-06 23:40:27 +0900

    design: 하루한컷 업로드 백헤더 text-white추가

---
+++ src/app/day-story/upload/page.tsx
@@ -0,0 +1,11 @@
+import { BackHeader } from "@/components/common/BackHeader";
+
+//하루한컷 업로드 페이지
+const DayStoryUpload = () => {
+  return (
+    <div className="w-full bg-black">
+      <BackHeader title="하루한컷" titleColor="white" />
+    </div>
+  );
+};
+export default DayStoryUpload;
