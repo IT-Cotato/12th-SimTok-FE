d07365282cae79390707f2d83e5905efc2c109a8
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-06 17:26:00 +0900

    feat: 공유일기 댓글창에서 인풋필드 클릭시 댓글 페이지로 이동

--- src/components/common/MessageInput.tsx
+++ src/components/common/MessageInput.tsx
@@ -2,17 +2,22 @@ import { useState } from "react";
 
 import { SendButton } from "@/assets/SendButton";
 
-export const MessageInput = () => {
+interface MessageInputProps {
+  onClick?: React.MouseEventHandler<HTMLInputElement>;
+}
+
+export const MessageInput = ({ onClick }: MessageInputProps) => {
   const [text, setText] = useState("");
   const hasText = Boolean(text);
 
   return (
     <div className="relative flex-1">
       <input
-        className="border-neutral-07 bg-neutral-11 w-full rounded-2xl border border-solid px-4 py-1 focus:outline-none"
+        className="border-neutral-07 bg-neutral-11 focus:border-mint-01 w-full rounded-2xl border border-solid px-4 py-1 focus:outline-none"
         type="text"
         value={text}
         onChange={e => setText(e.target.value)}
+        onClick={onClick}
       />
       <SendButton hasText={hasText} />
     </div>
