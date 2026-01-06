64d2780816989d3b227e5b5bee239351c805601e
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-06 14:42:06 +0900

    rename: 변수명 소문자로수정

--- src/app/shared-diary/[id]/page.tsx
+++ src/app/shared-diary/[id]/page.tsx
@@ -13,8 +13,8 @@ const SharedDiaryPage = () => {
   const { id } = useParams<{ id: string }>();
   const numericId = Number(id);
 
-  const SharedDiary = SharedDiaryData as Diary[];
-  const diaryData = SharedDiary.find(item => item.id === numericId);
+  const sharedDiary = SharedDiaryData as Diary[];
+  const diaryData = sharedDiary.find(item => item.id === numericId);
 
   if (!diaryData) {
     return <div>일기를 찾을 수 없습니다.</div>;
