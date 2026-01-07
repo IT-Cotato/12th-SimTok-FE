7878f980e22efda8402d35e4360c5e3587a8979e
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-07 20:06:18 +0900

    refactor: 진행단계바 코드 리팩토링

---
+++ src/components/common/ProgressDot.tsx
@@ -0,0 +1,23 @@
+interface progressDotsProps {
+  total: number;
+  current: number;
+}
+
+export default function ProgressDots({ total, current }: progressDotsProps) {
+  return (
+    <div className="z-10 flex w-full items-center justify-center px-4 py-[10px]">
+      <div className="flex gap-[9px]">
+        {Array.from({ length: total }).map((_, i) => {
+          const isActive = i <= current;
+
+          return (
+            <span
+              key={i}
+              className={`${isActive ? "bg-mint-01" : "bg-neutral-08"} h-3 w-3 rounded-full`}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
