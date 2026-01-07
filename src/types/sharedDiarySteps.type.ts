16b4e42c9e6c2df74260eda4c7511037045eef06
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-07 20:35:20 +0900

    refactor: 불필요한 타입 선언 제거

--- src/types/sharedDiarySteps.type.ts
+++ src/types/sharedDiarySteps.type.ts
@@ -1,29 +1,5 @@
 import { Emotion } from "./emotion.type";
 
-export type SharedDiaryStepContextMap = {
-  // 1. 이모지 선택
-  emotion: {
-    emotion?: Emotion;
-  };
-
-  // 2. 사진 + 텍스트 입력 (둘 다 필수)
-  write: {
-    emotion: Emotion;
-    content?: string;
-    file?: File;
-  };
-
-  // 3. 업로드 전 확인
-  confirm: {
-    emotion: Emotion;
-    content: string;
-    file: File;
-  };
-
-  // 4. 업로드 완료
-  complete: Record<string, never>;
-};
-
 export type SharedDiaryFormState = {
   emotion?: Emotion;
   content?: string;
