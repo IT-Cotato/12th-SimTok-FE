a3908a1cf25afe2732347275d1e76ba76c48d6da
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-09 02:07:49 +0900

    feat: 글쓰기 작성페이지 추가 밑 작성한 글 표시

--- src/components/sharedDiary/ContentStepButton.tsx
+++ src/components/sharedDiary/ContentStepButton.tsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+
 import { GalleryAssets } from "@/assets/GalleryIcon";
 
 import { useImageUpload } from "@/hooks/useImageUpload";
 
 import { InfoMessage } from "../dailyRecord/InfoMessage";
+import { WritePage } from "./WritePage";
 
 interface WriteStepButtonProps {
   onSelectImage: (file: File) => void;
   showInfoMessage: boolean;
+  text: string;
+  onChangeText: (value: string) => void;
 }
 export const WriteStepButton = ({
   onSelectImage,
   showInfoMessage,
+  text,
+  onChangeText,
 }: WriteStepButtonProps) => {
   const { inputRef, openFilePicker, onChangeFile } = useImageUpload({
     onSelect: ({ file }) => {
@@ -18,9 +25,10 @@ export const WriteStepButton = ({
     },
     maxSizeMB: 10,
   });
+  const [openTextPage, setOpenTextPage] = useState(false);
 
   return (
-    <section className="flex flex-col gap-[7.8px]">
+    <section className="relative flex flex-col gap-[7.8px]">
       {showInfoMessage && (
         <div className="px-4">
           <InfoMessage
@@ -42,7 +50,10 @@ export const WriteStepButton = ({
           </div>
           <p>사진추가하기</p>
         </button>
-        <button className="bg-neutral-11 border-mint-01 h-[95px] max-w-[196px] flex-1 cursor-pointer rounded-2xl border border-solid px-[10px] py-[10px] pt-[20px]">
+        <button
+          className="bg-neutral-11 border-mint-01 h-[95px] max-w-[196px] flex-1 cursor-pointer rounded-2xl border border-solid px-[10px] py-[10px] pt-[20px]"
+          onClick={() => setOpenTextPage(true)}
+        >
           <div className="text-sub2-sb inline-flex items-center justify-center rounded-2xl bg-white p-[10px] text-black">
             TEXT
           </div>
@@ -56,6 +67,17 @@ export const WriteStepButton = ({
         className="hidden"
         onChange={onChangeFile}
       />
+      {openTextPage && (
+        <div className="fixed inset-0 z-[100] flex justify-center bg-black/0">
+          <div className="h-full w-full max-w-[440px] bg-white">
+            <WritePage
+              onClose={() => setOpenTextPage(false)}
+              text={text}
+              onChangeText={onChangeText}
+            />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
