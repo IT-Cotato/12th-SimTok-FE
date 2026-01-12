a5ff1e420cfe43b23574f4289a9a9dffe2f8865d
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-13 00:27:17 +0900

    feat: 이미지 업로드 후 하루한컷 작성 상태 완료로 상태 내려줌

--- src/app/day-story/upload/page.tsx
+++ src/app/day-story/upload/page.tsx
@@ -1,15 +1,21 @@
+"use client";
+import { useState } from "react";
+
 import { BackHeader } from "@/components/common/BackHeader";
 import { DailyMissionCard } from "@/components/dailyPhoto/DailyMissionCard";
 import { DailyMissionProgress } from "@/components/dailyPhoto/DailyMissionProgress";
 
-//하루한컷 업로드 페이지
+import { MISSION_STATUS } from "@/constants/missionCard";
+
 const DayStoryUpload = () => {
+  const [status, setStatus] =
+    useState<keyof typeof MISSION_STATUS>("NOT_STARTED");
   return (
     <main className="w-full bg-black">
       <BackHeader title="하루한컷" titleColor="white" />
-      <DailyMissionProgress />
+      <DailyMissionProgress status={status} />
       <div className="mt-[143px] flex items-center justify-center">
-        <DailyMissionCard />
+        <DailyMissionCard status={status} setStatus={setStatus} />
       </div>
     </main>
   );
--- src/components/dailyPhoto/DailyMissionProgress.tsx
+++ src/components/dailyPhoto/DailyMissionProgress.tsx
@@ -4,29 +4,44 @@ import CheckIcon from "@/assets/check.svg";
 import CrossIcon from "@/assets/close.svg";
 import QuestionIcon from "@/assets/questionMark.svg";
 
-import { WEEK_DAYS } from "@/constants/dayToKorean";
+import { MISSION_STATUS } from "@/constants/missionCard";
+import { WEEK_DAYS_KOR, WEEK_DAY_KEYS } from "@/constants/weekDays";
+
+import dailyRecordProgress from "@/mock/dailyRecordProgress.json";
 
 import { getWeekDayStatus } from "@/utils/getCurrentDay";
 
-export const DailyMissionProgress = () => {
+type WeekDayKey = keyof typeof dailyRecordProgress;
+
+interface DailyMissionProgressProps {
+  status: keyof typeof MISSION_STATUS;
+}
+
+export const DailyMissionProgress = ({ status }: DailyMissionProgressProps) => {
   return (
     <ul className="scrollbar-hide mt-8 flex gap-[9px] overflow-x-auto px-4">
-      {WEEK_DAYS.map((item, index) => {
-        const status = getWeekDayStatus(index); // past | today | future
-        const isCompleted = false;
+      {WEEK_DAYS_KOR.map((item, index) => {
+        const dayStatus = getWeekDayStatus(index); // past | today | future
+        const dayKey = WEEK_DAY_KEYS[index] as WeekDayKey;
+
+        const isCompleted =
+          dayStatus === "today" && status === "IMAGE_CONFIRMED"
+            ? true
+            : dailyRecordProgress[dayKey];
 
         const showCheck =
-          (status === "past" && isCompleted) ||
-          (status === "today" && isCompleted);
+          (dayStatus === "past" && isCompleted) ||
+          (dayStatus === "today" && isCompleted);
 
-        const showCross = status === "past" && !isCompleted;
+        const showCross = dayStatus === "past" && !isCompleted;
+
         const showQuestion =
-          status === "future" || (status === "today" && !isCompleted);
+          dayStatus === "future" || (dayStatus === "today" && !isCompleted);
 
         return (
           <li key={item} className="h-[82px] w-[74px] shrink-0">
             <div
-              className={`${showQuestion ? "bg-white" : "bg-spring-01"} ${status != "future" ? "border-mint-01" : "border-neutral-08"} flex h-full flex-col items-center justify-center gap-[3px] rounded-2xl border border-solid px-[10px] py-[6px]`}
+              className={`flex h-full flex-col items-center justify-center gap-[3px] rounded-2xl border border-solid px-[10px] py-[6px] ${showQuestion ? "bg-white" : "bg-spring-01"} ${dayStatus !== "future" ? "border-mint-01" : "border-neutral-08"} `}
             >
               {showCheck && (
                 <div className="bg-mint-01 flex h-8 w-8 items-center justify-center rounded-full">
@@ -42,16 +57,20 @@ export const DailyMissionProgress = () => {
 
               {showQuestion && (
                 <div
-                  className={`${status === "today" ? "bg-mint-01" : "bg-neutral-08"} flex h-8 w-8 items-center justify-center rounded-full`}
+                  className={`flex h-8 w-8 items-center justify-center rounded-full ${dayStatus === "today" ? "bg-mint-01" : "bg-neutral-08"} `}
                 >
                   <QuestionIcon
-                    className={`${status === "today" ? "text-white" : "text-neutral-06"} h-6 w-6`}
+                    className={`h-6 w-6 ${
+                      dayStatus === "today" ? "text-white" : "text-neutral-06"
+                    } `}
                   />
                 </div>
               )}
 
               <span
-                className={`text-sub1-sb ${status != "future" ? "text-neutral-01" : "text-neutral-06"}`}
+                className={`text-sub1-sb ${
+                  dayStatus !== "future" ? "text-neutral-01" : "text-neutral-06"
+                } `}
               >
                 {item}
               </span>
