4d92f66507b2e8a51ae0ea399c6e08a893e5afe0
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-14 21:12:03 +0900

    feat: 로고있는 홈헤더 추가

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,1 +1,33 @@
-export const Header = () => {};
+"use client";
+import { useState } from "react";
+
+import AlarmIcon from "@/assets/bell.svg";
+import FriendIcon from "@/assets/friend.svg";
+
+export const Header = () => {
+  const [isAlarmNew, setIsAlarmNew] = useState(true);
+  return (
+    <header className="flex items-center justify-between px-4 py-[10px]">
+      <div className="text-h1 text-mint-01">SIMTALK</div>
+
+      <div className="flex gap-6">
+        <div>
+          <FriendIcon className="text-neutral-03" />
+        </div>
+        <div>
+          {isAlarmNew ? (
+            <button
+              className="relative cursor-pointer"
+              onClick={() => setIsAlarmNew(false)}
+            >
+              <AlarmIcon className="text-neutral-03 h-6 w-6 cursor-pointer" />
+              <span className="bg-red-00 absolute top-[2px] left-[14px] h-[10px] w-[10px] rounded-full" />
+            </button>
+          ) : (
+            <AlarmIcon className="text-neutral-03 h-6 w-6 cursor-pointer" />
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
