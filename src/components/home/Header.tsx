326bf5f363264339fea903b5a6a768045318b599
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-15 18:38:08 +0900

    refactor: 커서포인터 추가

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { useRouter } from "next/navigation";
+
 import { useState } from "react";
 
 import AlarmIcon from "@/assets/bell.svg";
@@ -6,18 +8,19 @@ import FriendIcon from "@/assets/friend.svg";
 
 export const Header = () => {
   const [isAlarmNew, setIsAlarmNew] = useState(true);
+  const router = useRouter();
   return (
     <header className="flex items-center justify-between px-4 py-[10px]">
       <div className="text-h1 text-mint-01">SIMTALK</div>
 
-      <div className="flex gap-6">
-        <div>
-          <FriendIcon className="text-neutral-03" />
-        </div>
+      <div className="flex items-center justify-center gap-6">
+        <button onClick={() => router.push("/friends")}>
+          <FriendIcon className="text-neutral-03 cursor-pointer" />
+        </button>
         <div>
           {isAlarmNew ? (
             <button
-              className="relative cursor-pointer"
+              className="relative flex cursor-pointer items-center justify-center"
               onClick={() => setIsAlarmNew(false)}
             >
               <AlarmIcon className="text-neutral-03 h-6 w-6 cursor-pointer" />
