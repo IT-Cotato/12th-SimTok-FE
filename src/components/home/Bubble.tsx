d7d0f9daef43e362184e2a8a030eddba1a1bcfb6
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-15 14:46:30 +0900

    feat: 식물상태에 따른 버블색 지정

---
+++ src/components/home/Bubble.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+import BlueCircle from "@/assets/home/blue_circle.svg";
+import GreenCircle from "@/assets/home/green-circle.svg";
+import RedCircle from "@/assets/home/red_circle.svg";
+import YellowCircle from "@/assets/home/yellow-circle.svg";
+
+import {
+  PlantWaterStatus,
+  PlantWaterStatusType,
+} from "@/constants/plantStatus";
+
+interface BubbleProps {
+  status: PlantWaterStatusType;
+}
+export const Bubble = ({ status }: BubbleProps) => {
+  switch (status) {
+    case PlantWaterStatus.EMPTY: {
+      return <YellowCircle />;
+    }
+    case PlantWaterStatus.WATERED_RECENTLY: {
+      return <GreenCircle />;
+    }
+    case PlantWaterStatus.WATERABLE: {
+      return <BlueCircle />;
+    }
+    case PlantWaterStatus.WITHERED: {
+      return <RedCircle />;
+    }
+  }
+};
