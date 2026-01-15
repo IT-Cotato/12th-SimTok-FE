c9aa49d0ed40254ad22ece646e68dfc049407fa8
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-15 18:46:29 +0900

    design: 서치필드 마이크 아이콘 위치 수정 및 placeholder부분 디자인 수정

--- src/components/home/SearchField.tsx
+++ src/components/home/SearchField.tsx
@@ -3,12 +3,16 @@ import SearchIcon from "@/assets/search.svg";
 
 export const SearchField = () => {
   return (
-    <div className="bg-neutral-11 relative w-full px-4 py-2">
-      <div className="flex items-center gap-[10px]">
+    <div className="bg-neutral-11 relative flex w-full items-center rounded-2xl px-4 py-2">
+      <div className="flex flex-1 items-center gap-[10px] pr-8">
         <SearchIcon className="text-neutral-07 h-5 w-5" />
-        <input type="text" placeholder="친구이름을 검색해보세요"></input>
+        <input
+          type="text"
+          placeholder="친구이름을 검색해보세요"
+          className="w-full focus:outline-none"
+        ></input>
       </div>
-      <MicIcon className="text-neutral-07 absolute right-0 h-5 w-5" />
+      <MicIcon className="text-neutral-07 absolute top-1/2 right-4 h-6 w-6 -translate-y-1/2" />
     </div>
   );
 };
