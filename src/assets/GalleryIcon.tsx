99e3855b45013ad59f4bb99ba6e58ba70a617cd0
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-15 15:15:16 +0900

    feat: z index 유효한 숫자로 변경

--- src/assets/GalleryIcon.tsx
+++ src/assets/GalleryIcon.tsx
@@ -2,9 +2,9 @@ import GalleryIcon from "@/assets/gallery.svg";
 
 export const GalleryAssets = () => {
   return (
-    <div className="flex">
-      <GalleryIcon className="z-2 h-[15px] w-[15px]" />
-      <div className="bg-orange-00 text-sub2-sb z-1 -ml-[3px] flex h-[15px] w-[15px] items-center justify-center rounded-full">
+    <div className="flex" role="img" aria-label="갤러리 아이콘">
+      <GalleryIcon className="z-20 h-[15px] w-[15px]" />
+      <div className="bg-orange-00 text-sub2-sb z-10 -ml-[3px] flex h-[15px] w-[15px] items-center justify-center rounded-full">
         👀
       </div>
       <div className="bg-green-01 text-sub3 -ml-[3px] flex h-[15px] w-[15px] items-center justify-center rounded-full">
--- src/components/dailyRecord/InfoMessage.tsx
+++ src/components/dailyRecord/InfoMessage.tsx
@@ -8,7 +8,7 @@ export const InfoMessage = ({ text, triangleUp = true }: InfoMessageProps) => {
       {triangleUp && (
         <div className="border-b-neutral-01 mx-[10px] -mb-[6px] h-0 w-0 self-end border-t-0 border-r-[9px] border-b-[18px] border-l-[9px] border-solid border-t-transparent border-r-transparent border-l-transparent"></div>
       )}
-      <div className="bg-neutral-01 text-sub2-sb border-neutral-01 z-1 rounded-[100px] border px-[10px] py-2 text-white">
+      <div className="bg-neutral-01 text-sub2-sb border-neutral-01 z-10 rounded-[100px] border px-[10px] py-2 text-white">
         {text}
       </div>
       {!triangleUp && (
