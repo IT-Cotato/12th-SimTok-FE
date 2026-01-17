1be2b76db5dc77f6bf8e3140bc9fd3f13cc83fc6
idid10 <ejwuud2@naver.com>
2026-01-17 20:28:42 +0900

    design: 마이페이지 홈 상단 프로필부분 수정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -5,17 +5,26 @@ import { useRouter } from "next/navigation";
 import { BackHeader } from "@/components/common/BackHeader";
 import { NavBar } from "@/components/common/NavBar";
 import { MenuItem } from "@/components/mypage/MenuItem";
+import { MyProfileCard } from "@/components/mypage/MyProfileCard";
 
 import { MY_PAGE_MENU_ITEMS } from "@/constants/mypage";
 
+import { useUserProfile } from "@/hooks/useUserProfile";
+
 const MyPage = () => {
   const router = useRouter();
+  const { userProfileData } = useUserProfile();
 
   return (
     <main className="flex min-h-dvh w-full justify-center">
-      <div className="mt-[13px] flex h-full w-110 flex-col px-4">
+      <div className="mt-[13px] flex h-full w-110 flex-col">
         <BackHeader title="마이페이지" />
         <section className="mt-[18.5px]">
+          {userProfileData && (
+            <MyProfileCard userProfileData={userProfileData} />
+          )}
+        </section>
+        <section className="mt-4">
           {MY_PAGE_MENU_ITEMS.map(item => (
             <MenuItem
               key={item.label}
