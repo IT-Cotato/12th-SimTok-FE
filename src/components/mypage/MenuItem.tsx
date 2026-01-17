3cbf14cebd9cfe656e4778542238045f34a452a5
idid10 <ejwuud2@naver.com>
2026-01-17 21:01:05 +0900

    feat: 마이페이지 홈 수정

--- src/components/mypage/MenuItem.tsx
+++ src/components/mypage/MenuItem.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { ReactNode } from "react";
+
 interface MenuItemProps {
   label: string;
   onClick: () => void;
+  Icon?: React.ElementType;
 }
 
-export const MenuItem = ({ label, onClick }: MenuItemProps) => (
+export const MenuItem = ({ label, onClick, Icon }: MenuItemProps) => (
   <button
     type="button"
-    className="border-neutral-10 text-h2 text-neutral-04 flex h-[71px] w-full cursor-pointer items-center border-b py-5"
+    className="text-sub0-sb text-neutral-03 flex h-8 w-full cursor-pointer items-center gap-2.5 px-4"
     onClick={onClick}
   >
-    {label}
+    {Icon && <Icon className="h-6 w-6" />} {/* 아이콘 렌더링 */}
+    <span>{label}</span>
   </button>
 );
