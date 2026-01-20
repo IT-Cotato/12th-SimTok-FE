7f3d3a903405538f0ad83f857da5c9f465a37be7
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-20 12:34:58 +0900

    design: 서치필드 클릭시 색상 변화 추가

--- src/components/friends/SearchField.tsx
+++ src/components/friends/SearchField.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
+
 import MicIcon from "@/assets/mic-stroke.svg";
 import SearchIcon from "@/assets/search.svg";
 
 export const SearchField = () => {
+  const [isClick, setIsClick] = useState(false);
   return (
-    <div className="bg-neutral-11 relative flex w-full items-center rounded-2xl px-4 py-2">
+    <div
+      className={`${isClick ? "border-mint-01 border bg-white" : "bg-neutral-11"} relative flex w-full items-center rounded-2xl px-4 py-2`}
+      onClick={() => setIsClick(true)}
+    >
       <div className="flex flex-1 items-center gap-[10px] pr-8">
-        <SearchIcon className="text-neutral-07 h-5 w-5" />
+        <SearchIcon
+          className={`${isClick ? "text-mint-01" : "text-neutral-07"} h-5 w-5`}
+        />
         <input
           type="text"
           placeholder="친구이름을 검색해보세요"
-          className="w-full focus:outline-none"
+          className="text-sub0 text-neutral-01 w-full focus:outline-none"
         ></input>
       </div>
-      <MicIcon className="text-neutral-07 absolute top-1/2 right-4 h-6 w-6 -translate-y-1/2" />
+      <MicIcon
+        className={`${isClick ? "text-neutral-02" : "text-neutral-07"} absolute top-1/2 right-4 h-6 w-6 -translate-y-1/2`}
+      />
     </div>
   );
 };
