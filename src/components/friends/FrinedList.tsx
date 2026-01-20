7236dd2cb341979357fd769c1997381aeb56d67d
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-20 14:14:28 +0900

    feat: 친구검색시, 전체친구수 안보이게 처리

--- src/components/friends/FrinedList.tsx
+++ src/components/friends/FrinedList.tsx
@@ -1,25 +1,50 @@
 import Image from "next/image";
 
+import { useState } from "react";
+
 import friendListData from "@/mock/friendList.json";
+
+import { FriendProfile } from "@/types/friendProfile.type";
+
+import { ProfileModal } from "./ProfileModal";
 
 interface FriendListProps {
   searchText: string;
+  setModalOpen: (open: boolean) => void;
 }
-export const FriendList = ({ searchText }: FriendListProps) => {
+export const FriendList = ({ searchText, setModalOpen }: FriendListProps) => {
   const filteredFriends = searchText
     ? friendListData.filter(user => user.userName.includes(searchText))
     : friendListData;
 
+  const [selectedFriend, setSelectedFriend] = useState<FriendProfile | null>(
+    null,
+  );
+
+  const profileModalOpen = (friend: FriendProfile) => {
+    setSelectedFriend(friend);
+    setModalOpen(true);
+  };
+
+  const profileModalClose = () => {
+    setSelectedFriend(null);
+    setModalOpen(false);
+  };
+
   return (
     <section>
-      <div className="flex gap-1 px-4">
-        <p className="text-sub1-r text-neutral-04">친구</p>
-        <p className="text-sub1-r text-neutral-04">{friendListData.length}</p>
-      </div>
+      {!searchText && (
+        <div className="flex gap-1 px-4">
+          <p className="text-sub1-r text-neutral-04">친구</p>
+          <p className="text-sub1-r text-neutral-04">{friendListData.length}</p>
+        </div>
+      )}
+
       {filteredFriends.map(friend => (
         <div
           key={friend.userId}
           className="hover:bg-neutral-10 flex cursor-pointer gap-4 px-4 py-[10px]"
+          onClick={() => profileModalOpen(friend)}
         >
           <Image
             src={friend.profileImg}
@@ -33,6 +58,14 @@ export const FriendList = ({ searchText }: FriendListProps) => {
           </div>
         </div>
       ))}
+      {selectedFriend && (
+        <ProfileModal
+          userId={selectedFriend.userId}
+          userName={selectedFriend.userName}
+          profileImg={selectedFriend.profileImg}
+          onClose={profileModalClose}
+        />
+      )}
     </section>
   );
 };
