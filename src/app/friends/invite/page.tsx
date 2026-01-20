f918eaf8b95a563b8c1cc469281e94cdb4f73d98
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-21 02:12:08 +0900

    feat: QR코드활성화, 초대코드 활성화에 따른 화면 분리

--- src/app/friends/invite/page.tsx
+++ src/app/friends/invite/page.tsx
@@ -1,14 +1,27 @@
 "use client";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 
+import { useEffect, useState } from "react";
+
+import { FullButton } from "@/components/common/FullButton";
 import { GlassStyleHeader } from "@/components/common/GlassStyleHeader";
+import { InputField } from "@/components/friends/InputField";
+import { QRCodeWrapper } from "@/components/friends/QRCodeWrapper";
 
 const FriendInvitePage = () => {
+  const router = useRouter();
+
   const [selectTitle, setSelectTitle] = useState<"left" | "right">("left");
+  const [textCodeMode, setTextCodeMode] = useState(false);
+  const [inputCode, setInputCode] = useState("");
+
+  useEffect(() => {
+    setTextCodeMode(selectTitle === "right");
+  }, [selectTitle]);
 
   return (
     <main
-      className={`${selectTitle === "left" ? "bg-radial-mint" : "bg-white"} w-full`}
+      className={`${selectTitle === "left" ? "bg-radial-mint" : inputCode.length > 0 ? "bg-radial-yellowgreen-mintgreen" : "bg-white"} w-full`}
     >
       <GlassStyleHeader
         backHeader={true}
@@ -19,13 +32,33 @@ const FriendInvitePage = () => {
         onChangeSelectTitle={setSelectTitle}
       />
       {selectTitle === "left" && (
-        <section>
+        <section className="flex flex-col gap-[103px]">
           <sub className="text-sub-number text-neutral-01 mt-[54.5px] flex items-center justify-center px-4 py-[10px]">
             QR코드를 친구에게 보여주세요!
           </sub>
+          <div className="relative px-4">
+            <QRCodeWrapper textCodeMode={textCodeMode} />
+          </div>
         </section>
       )}
-      {selectTitle === "right"}
+      {selectTitle === "right" && (
+        <section className="flex flex-col gap-[93px]">
+          <div className="mt-[44.5px] px-4 py-[10px]">
+            <InputField onChangeInputText={setInputCode} />
+          </div>
+          <div className="relative px-4">
+            <QRCodeWrapper textCodeMode={textCodeMode} />
+          </div>
+          <div className="fixed bottom-0 w-full max-w-[440px] bg-white px-4 py-[10px] pt-[10px]">
+            <FullButton
+              isActive={inputCode.length > 0}
+              onClick={() => router.push(`/friends/add/${inputCode}`)}
+            >
+              입력완료
+            </FullButton>
+          </div>
+        </section>
+      )}
     </main>
   );
 };
