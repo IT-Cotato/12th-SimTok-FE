cf24fbb8ea8bd8f9662700709cee206da794e5d8
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-20 16:57:49 +0900

    feat: edit모드에서 선택한 인원갯수 추가

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -8,11 +8,10 @@ import { SearchField } from "@/components/common/SearchField";
 import { FriendList } from "@/components/friends/FrinedList";
 
 const FriendsListPage = () => {
-  const [searchText, setSearchText] = useState("");
-  const [modalOpen, setModalOpen] = useState(false);
-  const [isEditMode, setIsEditMode] = useState(false);
-  const [selectedIds, setSelectedIds] = useState<number[]>([]);
-  const [isButtonClick, setIsButtonClick] = useState(false);
+  const [searchText, setSearchText] = useState(""); // 서치필드에 입력된 텍스트
+  const [modalOpen, setModalOpen] = useState(false); // 친구프로필 모달
+  const [isEditMode, setIsEditMode] = useState(false); // 편집모드 전환
+  const [selectedIds, setSelectedIds] = useState<number[]>([]); // 편집모드에서 선택한 friendId
 
   const toggleFriend = (userId: number) => {
     setSelectedIds(prev =>
@@ -29,7 +28,12 @@ const FriendsListPage = () => {
           <BackHeader
             title="친구목록"
             subtext="편집하기"
-            onClickEdit={() => setIsEditMode(prev => !prev)}
+            isEditMode={isEditMode}
+            onClickEdit={() => {
+              setIsEditMode(prev => !prev);
+              if (isEditMode) setSelectedIds([]);
+            }}
+            selectedCount={selectedIds.length}
           />
         </div>
       )}
@@ -46,13 +50,17 @@ const FriendsListPage = () => {
           onToggleFriend={toggleFriend}
         />
       </div>
-      <div className="fixed inset-x-0 bottom-[33px] z-50">
-        <div className="mx-auto w-full max-w-[440px]">
-          <button className="bg-mint-01 mr-4 ml-auto flex h-[70px] w-[70px] items-center justify-center rounded-full shadow-[0_0_10px_0_rgba(0,0,0,0.10)]">
-            <FriendAddIcon className="h-10 w-10 text-white" />
-          </button>
+      {isEditMode ? (
+        <div className="fixed"></div>
+      ) : (
+        <div className="fixed inset-x-0 bottom-[33px] z-50">
+          <div className="mx-auto w-full max-w-[440px]">
+            <button className="bg-mint-01 mr-4 ml-auto flex h-[70px] w-[70px] items-center justify-center rounded-full shadow-[0_0_10px_0_rgba(0,0,0,0.10)]">
+              <FriendAddIcon className="h-10 w-10 text-white" />
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 };
