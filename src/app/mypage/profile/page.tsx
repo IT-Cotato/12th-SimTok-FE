0840d6120f33ce81ad68a2fc7059ef56ee8c8efc
idid10 <ejwuud2@naver.com>
2026-01-20 23:31:07 +0900

    refactor:모달 활성화시 하단 버튼 안보이게 설정

--- src/app/mypage/profile/page.tsx
+++ src/app/mypage/profile/page.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from "next/navigation";
 
+import { useState } from "react";
+
 import { BackHeader } from "@/components/common/BackHeader";
 import { FullButton } from "@/components/common/FullButton";
 import { ProfileSummary } from "@/components/profile/ProfileSummary";
@@ -10,7 +12,9 @@ import { useUserProfile } from "@/hooks/useUserProfile";
 
 const ProfileSettingPage = () => {
   const router = useRouter();
-  const { userProfileData, error } = useUserProfile();
+  const { userProfileData } = useUserProfile();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <main className="relative flex min-h-dvh w-full justify-center bg-white">
@@ -18,17 +22,22 @@ const ProfileSettingPage = () => {
         <BackHeader title="프로필 설정" />
 
         <section className="flex flex-col items-center">
-          <ProfileSummary userProfileData={userProfileData} />
+          <ProfileSummary
+            userProfileData={userProfileData}
+            onModalStateChange={setIsModalOpen}
+          />
         </section>
       </div>
 
-      <div className="fixed right-0 bottom-0 left-0 z-10 px-4 pb-4">
-        <div className="mx-auto max-w-110">
-          <FullButton onClick={() => router.push("/mypage")}>
-            설정완료
-          </FullButton>
+      {!isModalOpen && (
+        <div className="fixed right-0 bottom-0 left-0 z-10 px-4 pb-4">
+          <div className="mx-auto max-w-110">
+            <FullButton onClick={() => router.push("/mypage")}>
+              설정완료
+            </FullButton>
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 };
