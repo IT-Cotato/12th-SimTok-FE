f8bafdc7477e5ffd8fd3f7eba366667dbbb1b130
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-22 15:43:44 +0900

    feat: 대화빈도 선택 모달 추가

--- src/components/friends/SettingField.tsx
+++ src/components/friends/SettingField.tsx
@@ -1,5 +1,6 @@
 "use client";
 import type { ReactNode } from "react";
+import { useState } from "react";
 
 import CheckIcon from "@/assets/check.svg";
 import DownArrow from "@/assets/down-arrow.svg";
@@ -20,9 +21,11 @@ import {
   ChatTopicItem,
 } from "@/types/friendProfile.type";
 
+import { ChatFrequencyModal } from "./ChatFrequencyModal";
+
 interface SettingFieldProps {
   userName: string;
-  goalDays?: number | undefined;
+  goalDays?: number;
   chatStyle?: ChatStyle;
   chatTopic: ChatTopic[];
   onChangeGoalDays: (value?: number) => void;
@@ -31,83 +34,118 @@ interface SettingFieldProps {
 }
 
 const getChatFrequencyLabel = (goalDays?: number) =>
-  CHAT_FREQUENCY_OPTIONS.find(o => o.value === goalDays)?.label ??
-  "선택하지 않음";
-
-const SETTINGS_SECTION_RENDERER: Record<
-  SettingsMenuKey,
-  (props: SettingFieldProps) => ReactNode
-> = {
-  friendName: ({ userName }) => (
-    <div className="bg-neutral-11 flex h-[55px] items-center gap-[10px] rounded-2xl px-[10px] py-2">
-      <ProfileIcon className="text-neutral-07 h-6 w-6" />
-      <span className="text-h2 text-neutral-07">{userName}</span>
-    </div>
-  ),
-
-  chatFrequency: ({ goalDays, onChangeGoalDays }) => (
-    <div className="bg-neutral-11 flex h-[55px] cursor-pointer items-center justify-between rounded-2xl px-[10px] py-2">
-      <span className="text-neutral-07 text-h2">
-        {getChatFrequencyLabel(goalDays)}
-      </span>
-      <DownArrow className="text-neutral-07 h-6 w-6" />
-    </div>
-  ),
-
-  chatStyle: ({ chatStyle, onChangeChatStyle }) => (
-    <div className="flex gap-[45px]">
-      {CHAT_STYLE.map(({ key, label }) => (
-        <div key={key} className="flex items-center gap-[10px]">
-          <button
-            className={`relative h-6 w-6 cursor-pointer rounded-full ${
-              chatStyle === key ? "bg-mint-01" : "border-neutral-08 border"
-            }`}
-            onClick={() => onChangeChatStyle(key)}
-          >
-            {chatStyle === key && (
-              <CheckIcon className="absolute inset-0 m-auto h-5 w-5 text-white" />
-            )}
-          </button>
-          <span
-            className={`${chatStyle === key ? "text-neutral-03" : "text-neutral-07"} text-h2`}
-          >
-            {label}
-          </span>
-        </div>
-      ))}
-    </div>
-  ),
-  chatTopic: ({ chatTopic, onToggleChatTopic }) => (
-    <div className="grid grid-cols-3 gap-[10px]">
-      {CHAT_TOPIC.map(({ key, label, icon }: ChatTopicItem) => (
-        <button
-          key={key}
-          className={`flex h-[55px] w-[127px] cursor-pointer items-center justify-center rounded-2xl border ${chatTopic.includes(key) ? "border-mint-01" : "border-neutral-08"}`}
-          onClick={() => onToggleChatTopic(key)}
-        >
-          {key === "custom" ? (
-            <PlusIcon className="h-6 w-6" />
-          ) : (
-            <>
-              {icon && <img src={icon} alt={label} className="h-6 w-6" />}
-              <p className="text-h2 text-neutral-01">{label}</p>
-            </>
-          )}
-        </button>
-      ))}
-    </div>
-  ),
-};
+  CHAT_FREQUENCY_OPTIONS.find(o => o.value === goalDays)?.label ?? "";
 
 export const SettingField = (props: SettingFieldProps) => {
-  const { userName, goalDays, chatStyle, chatTopic } = props;
+  const {
+    userName,
+    goalDays,
+    chatStyle,
+    chatTopic,
+    onChangeGoalDays,
+    onChangeChatStyle,
+    onToggleChatTopic,
+  } = props;
+
+  const [isFrequencyModalOpen, setIsFrequencyModalOpen] = useState(false);
+
+  const openFrequencySelectModal = () => {
+    setIsFrequencyModalOpen(true);
+  };
+
+  const closeFrequencySelectModal = () => {
+    setIsFrequencyModalOpen(false);
+  };
+
+  const SETTINGS_SECTION_RENDERER: Record<SettingsMenuKey, () => ReactNode> = {
+    friendName: () => (
+      <div className="bg-neutral-11 flex h-[55px] items-center gap-[10px] rounded-2xl px-[10px] py-2">
+        <ProfileIcon className="text-neutral-07 h-6 w-6" />
+        <span className="text-h2 text-neutral-07">{userName}</span>
+      </div>
+    ),
+
+    chatFrequency: () => (
+      <>
+        <div
+          className="bg-neutral-11 flex h-[55px] cursor-pointer items-center justify-between rounded-2xl px-[10px] py-2"
+          onClick={openFrequencySelectModal}
+        >
+          <span className="text-neutral-07 text-h2">
+            {getChatFrequencyLabel(goalDays)}
+          </span>
+          <DownArrow className="text-neutral-07 h-6 w-6" />
+        </div>
+
+        {isFrequencyModalOpen && (
+          <ChatFrequencyModal
+            value={goalDays}
+            onChange={days => {
+              onChangeGoalDays(days);
+              closeFrequencySelectModal();
+            }}
+            onClose={closeFrequencySelectModal}
+          />
+        )}
+      </>
+    ),
+
+    chatStyle: () => (
+      <div className="flex gap-[45px]">
+        {CHAT_STYLE.map(({ key, label }) => (
+          <div key={key} className="flex items-center gap-[10px]">
+            <button
+              className={`relative h-6 w-6 rounded-full ${
+                chatStyle === key ? "bg-mint-01" : "border-neutral-08 border"
+              }`}
+              onClick={() => onChangeChatStyle(key)}
+            >
+              {chatStyle === key && (
+                <CheckIcon className="absolute inset-0 m-auto h-5 w-5 text-white" />
+              )}
+            </button>
+            <span
+              className={`text-h2 ${
+                chatStyle === key ? "text-neutral-03" : "text-neutral-07"
+              }`}
+            >
+              {label}
+            </span>
+          </div>
+        ))}
+      </div>
+    ),
+
+    chatTopic: () => (
+      <div className="grid grid-cols-3 gap-[10px]">
+        {CHAT_TOPIC.map(({ key, label, icon }: ChatTopicItem) => (
+          <button
+            key={key}
+            className={`flex h-[55px] w-[127px] items-center justify-center rounded-2xl border ${
+              chatTopic.includes(key) ? "border-mint-01" : "border-neutral-08"
+            }`}
+            onClick={() => onToggleChatTopic(key)}
+          >
+            {key === "custom" ? (
+              <PlusIcon className="h-6 w-6" />
+            ) : (
+              <>
+                {icon && <img src={icon} alt={label} className="h-6 w-6" />}
+                <p className="text-h2 text-neutral-01">{label}</p>
+              </>
+            )}
+          </button>
+        ))}
+      </div>
+    ),
+  };
 
   return (
     <section className="flex flex-col gap-5">
       {FRIENDS_SETTINGS_MENU.map(({ key, title }) => (
         <div key={key} className="flex flex-col gap-[6px] px-4">
           <h1 className="text-h3 text-neutral-05">{title}</h1>
-          {SETTINGS_SECTION_RENDERER[key](props)}
+          {SETTINGS_SECTION_RENDERER[key]()}
         </div>
       ))}
     </section>
