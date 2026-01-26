4f3616c1060e8d63c0f35b99e89aa1c38575c1c6
idid10 <ejwuud2@naver.com>
2026-01-26 22:39:22 +0900

    design: 회원가입 비밀번호 설정 페이지 UI 수정 완료

--- src/app/signup/password/page.tsx
+++ src/app/signup/password/page.tsx
@@ -4,18 +4,15 @@ import { useRouter } from "next/navigation";
 
 import { useState } from "react";
 
-import Exclamation from "@/assets/exclamation.svg";
-import EyeIcon from "@/assets/eye.svg";
+import Exclamation from "@/assets/exclamation-circle.svg";
 import LockIcon from "@/assets/lock.svg";
 
 import { BackHeader } from "@/components/common/BackHeader";
 import { FullButton } from "@/components/common/FullButton";
+import { InputField } from "@/components/common/InputField";
 import { PageTitle } from "@/components/common/PageTitle";
 
 import { usePasswordValidation } from "@/hooks/usePasswordValidation";
-
-const getIconClass = (state: string) =>
-  state === "invalid" ? "text-orange-00" : "text-neutral-07";
 
 const SettingPage = () => {
   const router = useRouter();
@@ -23,112 +20,100 @@ const SettingPage = () => {
   const [password, setPassword] = useState("");
   const [passwordConfirm, setPasswordConfirm] = useState("");
 
-  const [showPassword, setShowPassword] = useState(false);
-  const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
-
-  const { isValidPassword, getState, getBorderColor, getTextColor } =
-    usePasswordValidation();
+  const { isValidPassword, getState, getTextColor } = usePasswordValidation();
 
   const isPasswordValid = isValidPassword(password);
   const isPasswordConfirmValid =
     isValidPassword(passwordConfirm) && password === passwordConfirm;
 
-  const passwordState = getState(password, isPasswordValid);
-  const passwordConfirmState = getState(
-    passwordConfirm,
-    isPasswordConfirmValid,
-  );
+  const passwordState =
+    getState(password, isPasswordValid) === "empty"
+      ? "default"
+      : (getState(password, isPasswordValid) as
+          | "default"
+          | "invalid"
+          | "valid");
+
+  // const passwordState = getState(password, isPasswordValid) === "empty"
+  //   ? "default"
+  //   : getState(password, isPasswordValid);
+
+  // const passwordConfirmState = getState(passwordConfirm, isPasswordConfirmValid) === "empty"
+  //   ? "default"
+  //   : getState(passwordConfirm, isPasswordConfirmValid);
+
+  const passwordConfirmState =
+    getState(passwordConfirm, isPasswordConfirmValid) === "empty"
+      ? "default"
+      : (getState(passwordConfirm, isPasswordConfirmValid) as
+          | "default"
+          | "invalid"
+          | "valid");
 
   return (
-    <main className="flex min-h-dvh w-full justify-center bg-white">
-      <div className="mt-[13px] flex h-full w-110 flex-col">
-        <BackHeader title="회원가입" />
-
+    <main className="flex h-screen w-full justify-center bg-white">
+      <div className="flex w-full flex-col">
+        <BackHeader title="비밀번호 설정" />
         <PageTitle>
           로그인에 사용할 비밀번호를
           <br />
-          설정해주세요.
+          설정해주세요
         </PageTitle>
-
         <div className="mt-[29px] flex w-full flex-col gap-2.5 px-4">
-          {/* 비밀번호 */}
-          <div
-            className={`bg-neutral-11 relative flex h-[55px] w-full items-center rounded-2xl border px-2.5 py-2 ${getBorderColor(passwordState)} `}
-          >
-            <div className="pr-2.5">
-              <LockIcon />
-            </div>
-
-            <input
-              type={showPassword ? "text" : "password"}
+          <div className="flex flex-col">
+            <InputField
+              isPassword={true}
+              //type="password"
               value={password}
               onChange={e => setPassword(e.target.value)}
-              placeholder="8자 이상 입력해주세요"
-              className="placeholder:text-neutral-07 text-h2 w-full bg-transparent text-black outline-none"
+              placeholder="8자리 이상 입력"
+              Icon={LockIcon}
+              state={passwordState}
+              suffix={
+                <div className="flex items-center justify-center">
+                  <Exclamation
+                    className={`h-6 w-6 transition-colors ${
+                      passwordState === "invalid"
+                        ? "text-orange-00"
+                        : "text-neutral-07"
+                    }`}
+                  />
+                </div>
+              }
             />
-
-            <button
-              type="button"
-              onClick={() => setShowPassword(prev => !prev)}
+            <div
+              className={`text-sub1-r underline ${getTextColor(passwordState)}`}
             >
-              <EyeIcon
-                className={`transition-colors ${
-                  showPassword ? "text-mint-01" : "text-neutral-05"
-                }`}
-              />
-            </button>
-            <div className="absolute top-1/2 right-0 translate-x-full -translate-y-1/2">
-              <Exclamation className={getIconClass(passwordState)} />
+              비밀번호를 입력해주세요.
             </div>
           </div>
 
-          {/* 비밀번호를 입력해주세요. */}
-          <div
-            className={`flex justify-end text-[10px] underline ${getTextColor(
-              passwordState,
-            )}`}
-          >
-            비밀번호를 입력해주세요.
-          </div>
-
-          {/* 비밀번호 확인 */}
-          <div
-            className={`bg-neutral-11 relative flex h-[55px] w-full items-center rounded-2xl border px-2.5 py-2 ${getBorderColor(passwordConfirmState)} `}
-          >
-            <div className="pr-2.5">
-              <LockIcon />
-            </div>
-
-            <input
-              type={showPasswordConfirm ? "text" : "password"}
+          <div className="flex flex-col">
+            <InputField
+              isPassword={true}
+              //type="password"
               value={passwordConfirm}
               onChange={e => setPasswordConfirm(e.target.value)}
-              placeholder="비밀번호를 한번 더 입력해주세요"
-              className="placeholder:text-neutral-07 text-h2 w-full bg-transparent text-black outline-none"
+              placeholder="8자리 이상 입력"
+              Icon={LockIcon}
+              state={passwordConfirmState}
+              suffix={
+                <div className="flex items-center justify-center px-1">
+                  <Exclamation
+                    className={`h-6 w-6 transition-colors ${
+                      passwordConfirmState === "invalid"
+                        ? "text-orange-00"
+                        : "text-neutral-07"
+                    }`}
+                  />
+                </div>
+              }
             />
-
-            <button
-              type="button"
-              onClick={() => setShowPasswordConfirm(prev => !prev)}
+            <div
+              className={`text-sub1-r underline ${getTextColor(passwordConfirmState)}`}
             >
-              <EyeIcon
-                className={`transition-colors ${
-                  showPasswordConfirm ? "text-mint-01" : "text-neutral-05"
-                }`}
-              />
-            </button>
-            <div className="absolute top-1/2 right-0 translate-x-full -translate-y-1/2">
-              <Exclamation className={getIconClass(passwordConfirmState)} />
+              비밀번호를 입력해주세요.
             </div>
-          </div>
-
-          {/*한번 더 입력해주세요. */}
-          <div
-            className={`flex justify-end text-[10px] underline ${getTextColor(
-              passwordConfirmState,
-            )}`}
-          >
-            한번 더 입력해주세요.
           </div>
 
           <div className="mt-[320px] flex w-full justify-center">
@@ -136,7 +121,7 @@ const SettingPage = () => {
               isActive={isPasswordConfirmValid}
               onClick={() => router.push("/onboarding")}
             >
-              완료
+              설정완료
             </FullButton>
           </div>
         </div>
