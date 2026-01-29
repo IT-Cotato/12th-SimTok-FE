487bd9f6e588c08ebe2ef0d01f10ce277692d439
idid10 <ejwuud2@naver.com>
2026-01-29 22:52:52 +0900

    fix: messageinput에 내용입력시 전송 아이콘 생성

--- src/components/chat/ChatField.tsx
+++ src/components/chat/ChatField.tsx
@@ -5,15 +5,22 @@ import { useState } from "react";
 import CameraIcon from "@/assets/camera.svg";
 import ImageIcon from "@/assets/image.svg";
 import MicIcon from "@/assets/mic.svg";
+import SendIcon from "@/assets/send.svg";
 
 export const MessageInput = () => {
   const [message, setMessage] = useState("");
 
+  const isNotEmpty = message.trim().length > 0;
+
   return (
     <div className="flex w-full justify-center px-4">
       <div className="bg-neutral-10 flex min-h-[50px] w-full max-w-[440px] items-center rounded-2xl px-[5px]">
-        <div className="flex-shrink-0">
-          <CameraIcon className="text-neutral-02 h-10 w-10" />
+        <div className="flex flex-shrink-0 items-center justify-center">
+          <CameraIcon
+            className={`h-10 w-10 transition-colors ${
+              isNotEmpty ? "text-neutral-09" : "text-neutral-06"
+            }`}
+          />
         </div>
 
         <div className="ml-[10px] flex flex-1 items-center">
@@ -26,13 +33,24 @@ export const MessageInput = () => {
           />
         </div>
 
-        <div className="flex items-center">
-          <button className="flex-shrink-0">
-            <ImageIcon className="text-neutral-02 h-8 w-8" />
-          </button>
-          <button className="flex-shrink-0">
-            <MicIcon className="text-neutral-02 h-6 w-6" />
-          </button>
+        <div className="flex flex-shrink-0 items-center justify-end">
+          {isNotEmpty ? (
+            <button
+              className="flex h-10 w-14 items-center justify-center transition-transform active:scale-95"
+              onClick={() => setMessage("")} //전송 시 필드 초기화
+            >
+              <SendIcon className="h-full w-full object-contain" />
+            </button>
+          ) : (
+            <div className="flex items-center">
+              <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center">
+                <ImageIcon className="text-neutral-02 h-7 w-7" />
+              </button>
+              <button className="flex h-8 w-8 flex-shrink-0 items-center justify-center">
+                <MicIcon className="text-neutral-02 h-6 w-6" />
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </div>
