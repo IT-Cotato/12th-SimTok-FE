53ea85ea9e45c59b9f801400e7f98a5269606265
idid10 <ejwuud2@naver.com>
2026-01-29 22:32:01 +0900

    feat:chatfield 수정

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -12,7 +12,6 @@ import friendListData from "@/mock/friendList.json";
 
 const Chatting = () => {
   const router = useRouter();
-
   const params = useParams();
   const id = params?.id;
 
@@ -35,17 +34,19 @@ const Chatting = () => {
         </BackHeader>
 
         <section className="flex-1 overflow-y-auto px-4 pt-4 pb-32">
-          <div className="flex flex-col gap-4"></div>
+          <div className="flex flex-col gap-4">{/* 메시지 내용 렌더링 */}</div>
         </section>
 
-        <div className="pointer-events-none fixed bottom-[82px] left-0 z-40 flex w-full justify-center">
-          <div className="flex w-full max-w-[440px] justify-end px-4">
-            <button className="pointer-events-auto transition-transform active:scale-95">
-              <AiIcon />
-            </button>
+        <div className="fixed bottom-0 z-40 w-full max-w-[440px] bg-white pb-[52px]">
+          <div className="relative w-full">
+            <div className="absolute right-4 bottom-full mb-4">
+              <button className="transition-transform active:scale-95">
+                <AiIcon />
+              </button>
+            </div>
+            <MessageInput />
           </div>
         </div>
-        <MessageInput />
       </div>
     </main>
   );
