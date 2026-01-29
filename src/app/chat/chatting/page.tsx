5b931d90f0c66618daab4ab1ef2458e9215f63fc
idid10 <ejwuud2@naver.com>
2026-01-29 10:44:35 +0900

    feat:ChatField 생성

--- src/app/chat/chatting/page.tsx
+++ src/app/chat/chatting/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+
+import AiIcon from "@/assets/AI.svg";
+import MenuIcon from "@/assets/list.svg";
+
+import { MessageInput } from "@/components/chat/ChatField";
+import { BackHeader } from "@/components/common/BackHeader";
+
 const Chatting = () => {
-  return <div>채팅</div>;
+  return (
+    <main className="relative flex min-h-dvh w-full justify-center bg-white">
+      <div className="flex h-full w-full flex-col">
+        <BackHeader title="나문희">
+          <button className="flex items-center justify-center">
+            <MenuIcon />
+          </button>
+        </BackHeader>
+
+        <section className="flex-1 overflow-y-auto px-4 pt-4 pb-32">
+          <div className="flex flex-col gap-4"></div>
+        </section>
+
+        <div className="pointer-events-none fixed bottom-[82px] left-0 z-40 flex w-full justify-center">
+          <div className="flex w-full max-w-[440px] justify-end px-4">
+            <button className="pointer-events-auto transition-transform active:scale-95">
+              <AiIcon />
+            </button>
+          </div>
+        </div>
+        <MessageInput />
+      </div>
+    </main>
+  );
 };
 
 export default Chatting;
