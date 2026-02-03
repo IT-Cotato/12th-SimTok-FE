ea6992d5c711d83eaf229cf03cd9e9d2adce3894
lemoncurdyogurt <suhosinsu1@naver.com>
2026-02-03 22:52:38 +0900

    feat: 정원초대메세지 추가

--- src/app/garden/new/invite/confirm/page.tsx
+++ src/app/garden/new/invite/confirm/page.tsx
@@ -1,1 +1,37 @@
-const InvitePlantConfirm = () => {};
+"use client";
+import { useRouter } from "next/navigation";
+
+import { useGardenStore } from "@/stores/useGardenStore";
+
+import MailBox from "@/assets/garden/red-message-box.svg";
+
+import { FullButton } from "@/components/common/FullButton";
+
+const InvitePlantConfirm = () => {
+  //TODO: 저장된 정보 api post 호출 추가
+  const router = useRouter();
+  const resetForm = useGardenStore(state => state.resetForm);
+  const handleNext = () => {
+    resetForm();
+    router.push("/garden");
+  };
+  return (
+    <main className="flex h-full w-full flex-col">
+      <section className="flex flex-1 flex-col justify-center gap-[90px]">
+        <h1 className="text-d2 text-neutral-01 flex items-start px-4 py-[10px]">
+          친구에게 초대장을 보냈어요!
+          <br />
+          수락 후 키우기가 시작돼요🙌
+        </h1>
+        <div className="flex justify-center">
+          <MailBox className="h-[370px] w-[326px]" />
+        </div>
+      </section>
+
+      <section className="mb-[42px] px-4 py-[10px]">
+        <FullButton onClick={handleNext}>확인완료</FullButton>
+      </section>
+    </main>
+  );
+};
+export default InvitePlantConfirm;
