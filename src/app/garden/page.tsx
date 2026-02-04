fc245483f722f8d220841f23acd6a1f1f3266abd
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-04 16:55:34 +0900

    feat: 정원규칙모달 추가

--- src/app/garden/page.tsx
+++ src/app/garden/page.tsx
@@ -6,11 +6,13 @@ import { useState } from "react";
 import SupportIcon from "@/assets/support.svg";
 
 import { GlassStyleHeader } from "@/components/common/GlassStyleHeader";
+import { GardenRules } from "@/components/garden/GardenRules";
 
 const Garden = () => {
   const router = useRouter();
 
   const [selectTitle, setSelectTitle] = useState<"left" | "right">("left");
+  const [openRules, setOpenRules] = useState(false);
 
   const handleChangeSelectTitle = (value: "left" | "right") => {
     setSelectTitle(value);
@@ -30,10 +32,26 @@ const Garden = () => {
           selectTitle={selectTitle}
           onChangeSelectTitle={handleChangeSelectTitle}
         />
-        <button className="absolute top-1/2 right-4 -translate-y-1/2 p-2">
-          <SupportIcon />
+        <button
+          className="absolute top-1/2 right-4 -translate-y-1/2 p-2"
+          onClick={() => setOpenRules(true)}
+        >
+          <SupportIcon className="text-neutral-04 h-6 w-6 cursor-pointer" />
         </button>
       </div>
+      {openRules && (
+        <div
+          className="bg-neutral-01/83 fixed inset-y-0 left-1/2 z-[100] flex max-w-[440px] -translate-x-1/2 items-center justify-center px-4"
+          onClick={() => setOpenRules(false)}
+        >
+          <div
+            className="w-full max-w-[440px] rounded-2xl bg-white shadow-xl"
+            onClick={e => e.stopPropagation()}
+          >
+            <GardenRules modalClose={() => setOpenRules(false)} />
+          </div>
+        </div>
+      )}
     </main>
   );
 };
