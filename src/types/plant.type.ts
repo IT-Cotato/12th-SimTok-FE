096dc0ff0e6f1488b40017f842bb477ed37a3330
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-08 22:55:15 +0900

    refactor: 정원규칙 및 정원상태 재정립

--- src/types/plant.type.ts
+++ src/types/plant.type.ts
@@ -1,4 +1,4 @@
-export type GrowthStatus = "SEED" | "SPROUT" | "STEM" | "BUD" | "BLOOM";
+export type GrowthStage = "SEED" | "SPROUT" | "STEM" | "BUD" | "BLOOM";
 
 export type PlantSort =
   | "daisy"
@@ -7,10 +7,36 @@ export type PlantSort =
   | "sunflower"
   | "baby-breath";
 
+export type GardenState =
+  // 식물 없는 경우
+  | "EMPTY"
+  | "SEED_READY"
+  // 성장 중
+  | "GROWING"
+  // 물 관련
+  | "WATERED_RECENTLY"
+  | "WATERABLE"
+  // 시듦
+  | "WITHERED" // 시듬 1일차
+  | "NUTRITION_AVAILABLE" // 시듬 2일차
+  | "AFTER_NUTRITION" // 영양제 주고 난 후 물이 필요한 상태
+  // 완료
+  | "COMPLETED";
+
+/* 정원 상태, 제목 매핑 */
+export interface GardenTitleItem {
+  state: GardenState;
+  growthStage: GrowthStage | null;
+  title: string;
+}
+
+/* 정원 식물 정보 */
 export type GardenPlant = {
   id: number;
   nickname: string;
   plantSort: PlantSort;
-  growthStatus: GrowthStatus;
-  recentWateredTime: string; // ISO string
+  growthStatus: GrowthStage;
+  recentWateredTime: string;
+  lastWateredById: number;
+  gardenState: GardenState;
 };
