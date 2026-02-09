95024f8a5bd568fa3813939037799f2e8f41b6d0
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-10 00:24:44 +0900

    refactor: 배열로 들어오는 타이틀 대응 컴포넌트 구조 수정

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -1,12 +1,18 @@
 type PageTitleProps = {
-  children: React.ReactNode;
+  title?: readonly string[];
 };
 
-export const PageTitle = ({ children }: PageTitleProps) => {
+export const PageTitle = ({ title }: PageTitleProps) => {
+  const lines = Array.isArray(title) ? title : [title];
+  if (!title) return null;
   return (
     <div className="flex w-full items-center px-4 py-2.5">
       <h1 className="text-neutral-02 text-d2 z-80 whitespace-pre-line">
-        {children}
+        {lines.map((line, idx) => (
+          <span key={idx} className="block">
+            {line}
+          </span>
+        ))}
       </h1>
     </div>
   );
