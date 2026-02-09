b90d4f64b53fcdbaaf12f7df24ab97da8668c8ff
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-09 23:21:40 +0900

    refactor: 두줄 대응 타이틀 디자인요소 수정

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -5,7 +5,7 @@ type PageTitleProps = {
 export const PageTitle = ({ children }: PageTitleProps) => {
   return (
     <div className="flex w-full items-center px-4 py-2.5">
-      <h1 className="text-neutral-02 text-d2 z-80 whitespace-nowrap">
+      <h1 className="text-neutral-02 text-d2 z-80 whitespace-pre-line">
         {children}
       </h1>
     </div>
