d8c9a6169decf1b98dbc5bb88ce091c66a5606cc
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-10 00:44:30 +0900

    feat: 정원 키우기 액션버튼 추가

--- src/app/garden/care/page.tsx
+++ src/app/garden/care/page.tsx
@@ -7,6 +7,15 @@ import MenuIcon from "@/assets/list.svg";
 
 import { GlassStyleHeader } from "@/components/common/GlassStyleHeader";
 import { PageTitle } from "@/components/common/PageTitle";
+import { ProgressDots } from "@/components/common/ProgressDot";
+import { ActionButton } from "@/components/garden/care/ActionButton";
+import { EmptyPlant } from "@/components/garden/care/EmptyPlant";
+import { GardenCareContent } from "@/components/garden/care/GardenCareContent";
+import { ProgressSection } from "@/components/garden/care/ProgressSection";
+
+// import PlantData from "@/mock/plantProgress.json";
+
+import { GardenState, GrowthStage } from "@/types/plant.type";
 
 const GardenCare = () => {
   const router = useRouter();
@@ -20,7 +29,7 @@ const GardenCare = () => {
     }
   };
   return (
-    <main className="w-full">
+    <main className="flex min-h-screen flex-col">
       <div className="relative w-full items-center justify-center">
         <GlassStyleHeader
           leftText="정원"
@@ -33,12 +42,25 @@ const GardenCare = () => {
           <MenuIcon className="text-neutral-04 h-6 w-6 cursor-pointer" />
         </button>
       </div>
-      <section>
-        <PageTitle>
-          아직 키우고 있는 식물이 없어요!
-          <br /> 정원에서 식물을 골라보세요
-        </PageTitle>
+      {/* {PlantData ? (
+        PlantData.map((data, idx) => (
+          <section key={idx}>
+            <ProgressDots total={PlantData.length} current={idx} />
+            <GardenCareContent
+              growthStatus={data.growthStatus as GrowthStage}
+              gardenState={data.gardenState as GardenState}
+            />
+          </section>
+        ))
+      ) : ( */}
+      <section className="flex flex-1 flex-col pb-[17px]">
+        <EmptyPlant />
       </section>
+
+      <section className="mb-[130px] px-4">
+        <ProgressSection />
+      </section>
+      {/* )} */}
     </main>
   );
 };
