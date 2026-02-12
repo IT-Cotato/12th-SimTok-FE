c0e126aa0cdba7736a0008c7bf44b1777dfb05cd
idid10 <ejwuud2@naver.com>
2026-02-13 03:46:41 +0900

    refactor: 환경변수 누락시 에러 발생

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,5 @@
-export const BACKEND_BASE_URL = process.env.BACKEND_BASE_URL;
+const url = process.env.BACKEND_BASE_URL;
+if (!url) {
+  throw new Error("BACKEND_BASE_URL environment variable is not set");
+}
+export const BACKEND_BASE_URL = url;
