13406ca802057387dbb89e854772a723d24ad4ca
lemoncurdyogurt <suhosinsu1@naver.com>
2026-02-14 03:01:50 +0900

    feat: 공유일기 업로드 후 박수추가

--- src/components/sharedDiary/CompleteStep.tsx
+++ src/components/sharedDiary/CompleteStep.tsx
@@ -1,3 +1,5 @@
+import HandClap from "@/assets/clap.svg";
+
 import { FullButton } from "../common/FullButton";
 
 interface CompleteStepProps {
@@ -5,11 +7,18 @@ interface CompleteStepProps {
 }
 export const CompleteStep = ({ onClose }: CompleteStepProps) => {
   return (
-    <main className="flex w-full flex-col items-center justify-center">
-      <h1 className="text-h1 text-neutral-01">공유일기 작성이 완료되었어요!</h1>
-      <p className="text-sub2-r text-neutral-05">
-        하루기록페이지에서 공유한 일기를 확인 할 수 있어요.
-      </p>
+    <main className="flex h-full w-full flex-col items-center justify-center">
+      <section className="mt-[119px] flex flex-col items-center gap-1">
+        <h1 className="text-d2 text-neutral-01">
+          공유일기 작성이 완료되었어요!
+        </h1>
+        <p className="text-h3 text-neutral-05">
+          하루기록페이지에서 공유한 일기를 확인 할 수 있어요.
+        </p>
+      </section>
+      <section className="mb-[112px] flex flex-1 items-center justify-center">
+        <HandClap className="h-[295px] w-[295px]" />
+      </section>
       <div className="fixed bottom-0 h-[119px] w-screen max-w-[440px] bg-white px-4 py-[10px]">
         <FullButton onClick={onClose}>
           <p>확인</p>
