017c6855dd39c5ca671796f70a8201659ef704f0
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-14 00:07:18 +0900

    design: 작성단계, 확정단계 변경 디자인 반영

--- src/components/sharedDiary/ContentStep.tsx
+++ src/components/sharedDiary/ContentStep.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 
 import { useEffect, useMemo, useState } from "react";
 
+import CloseIcon from "@/assets/close-bold.svg";
+
 import { Emotion } from "@/types/emotion.type";
 
 import { getEmotionMeta } from "@/utils/getEmotionMeta";
@@ -33,6 +35,8 @@ export const ContentStep = ({
   const [text, setText] = useState<string>(defaultContent || "");
 
   const hasInput = Boolean(text || file);
+  const hasText = text.trim().length > 0;
+  const hasImage = !!file;
 
   const previewUrl = useMemo(() => {
     if (!file) return null;
@@ -59,50 +63,64 @@ export const ContentStep = ({
           subTitle="공유일기를 작성해보세요."
         />
       </div>
-      {emotionData && (
-        <section className="flex flex-col items-center justify-center gap-[6px]">
-          <Image
-            src={emotionData.imageSrc}
-            width={96}
-            height={96}
-            alt="selected-emotion"
-            className="h-24 w-24"
-          />
-          <p className="text-sub1-r text-black">
-            &ldquo;오늘 하루는 {emotionData.presentText}&rdquo;
-          </p>
-        </section>
-      )}
-
-      {(previewUrl || text) && (
-        <section className="pb-[254px]">
-          {previewUrl && (
+      <section className="flex flex-col gap-4">
+        {emotionData && (
+          <section className="flex flex-col items-center justify-center">
             <Image
-              src={previewUrl}
-              alt="업로드 이미지 미리보기"
-              width={440}
-              height={440}
-              className="w-full"
+              src={emotionData.imageSrc}
+              width={hasInput ? 47 : 96}
+              height={hasInput ? 47 : 96}
+              alt="selected-emotion"
             />
-          )}
+            <p className="text-sub1-r text-black">
+              &ldquo;오늘 하루는 {emotionData.presentText}&rdquo;
+            </p>
+          </section>
+        )}
+        <section className="flex flex-col">
+          {hasInput && (
+            <section>
+              {previewUrl && (
+                <figure className="relative mx-4">
+                  <Image
+                    src={previewUrl}
+                    alt="업로드 이미지 미리보기"
+                    width={440}
+                    height={440}
+                    className="w-full rounded-2xl object-cover"
+                  />
+                  <button
+                    className="bg-mint-01 absolute top-[6px] right-2 flex h-6 w-6 cursor-pointer items-center justify-center rounded-full"
+                    onClick={() => setFile(undefined)}
+                  >
+                    <CloseIcon className="h-2 w-2 text-white" />
+                  </button>
+                </figure>
+              )}
 
-          {text && (
-            <div className="text-sub1-r px-4 py-[15px] whitespace-pre-wrap text-black">
-              {text}
-            </div>
+              {text && (
+                <div className="text-body1-md px-4 py-[15px] break-words whitespace-pre-wrap text-black">
+                  {text}
+                </div>
+              )}
+            </section>
           )}
         </section>
-      )}
+      </section>
 
-      <div className="fixed bottom-[119px] z-99 w-full max-w-[440px]">
+      <div
+        className={`${hasInput ? "pb-[119px]" : "fixed bottom-[119px]"} w-full max-w-[440px]`}
+      >
         <WriteStepButton
           onSelectImage={selectedFile => setFile(selectedFile)}
           showInfoMessage={!hasInput}
           text={text}
           onChangeText={setText}
+          hasImage={hasImage}
+          hasText={hasText}
         />
       </div>
-      <div className="fixed bottom-0 h-[119px] w-screen max-w-[440px] bg-white px-4 py-[10px]">
+      <div className="border-neutral-09 fixed bottom-0 z-90 h-[119px] w-screen max-w-[440px] bg-white px-4 py-[10px]">
         <FullButton
           isActive={hasInput}
           onClick={() => {
