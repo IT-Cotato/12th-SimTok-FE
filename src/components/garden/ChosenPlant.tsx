5e002d2e1fe28174a1200a959c7e3c4de71ba0fc
lemoncurdyogurt <suhosinsu1@naver.com>
2026-02-18 02:05:41 +0900

    feat: 정원 초대 모달 추가

--- src/components/garden/ChosenPlant.tsx
+++ src/components/garden/ChosenPlant.tsx
@@ -1,35 +1,55 @@
 import Image from "next/image";
 
+import { b } from "framer-motion/client";
+
 import { PLANT_SORT_INFO } from "@/constants/garden/plantList";
 
+import { PlantSort } from "@/types/plant.type";
+
 import { NameInput } from "../common/NameInput";
+import { OnlyLoader } from "../common/OnlyLoader";
 
 export type Plant = (typeof PLANT_SORT_INFO)[number];
 
 interface ChosenPlantProps {
-  plant: Plant;
+  plantSort: PlantSort;
   nickname: string;
   onChangeNickname?: (value: string) => void;
   isNicknameEditable?: boolean;
+  width?: number;
+  height?: number;
+  bgWidth?: number;
 }
 
 export const ChosenPlant = ({
   nickname,
-  plant,
+  plantSort,
   onChangeNickname,
   isNicknameEditable = false,
+  width = 178,
+  height = 239,
+  bgWidth = 160,
 }: ChosenPlantProps) => {
+  const plantInfo = PLANT_SORT_INFO.find(info => info.id === plantSort);
+
+  if (!plantInfo) {
+    return <OnlyLoader />;
+  }
+
   return (
     <section className="flex flex-col gap-4">
       <div className="relative flex justify-center">
         {/* 배경 */}
-        <div className={`${plant.bgColor} h-40 w-40 rounded-[36px]`} />
+        <div
+          className={`${plantInfo?.bgColor} rounded-[36px]`}
+          style={{ width: `${bgWidth}px`, height: `${bgWidth}px` }}
+        />
 
         {/* 이미지 */}
         <Image
-          src={plant.img}
-          width={178}
-          height={239}
+          src={plantInfo?.img}
+          width={width}
+          height={height}
           alt="선택한 식물 이미지"
           className="absolute bottom-0"
         />
@@ -37,7 +57,7 @@ export const ChosenPlant = ({
       <NameInput
         value={nickname}
         onChange={onChangeNickname}
-        placeholder={plant.name}
+        placeholder={plantInfo?.name}
         changeable={isNicknameEditable}
       />
     </section>
