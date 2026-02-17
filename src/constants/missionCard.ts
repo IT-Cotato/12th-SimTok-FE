ded1793e9799cf87bc42200193d95fdfb1fd4354
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-17 20:00:42 +0900

    design: 미션카드 상태에 따른 카드 변경된 디자인 반영

--- src/constants/missionCard.ts
+++ src/constants/missionCard.ts
@@ -3,17 +3,14 @@ import { MissionCategory } from "@/types/dailyRecord.type";
 export const MISSION_STATUS = {
   NOT_STARTED: {
     title: "하루한컷 미션 도착!",
-    subtitle: (localday: string) => localday,
     buttonText: "시작하기",
   },
   IMAGE_UPLOADED: {
-    title: "하루한컷 미션을 공유할까요?",
-    subtitle: "확인을 누르면 미션이 완료돼요",
+    title: "하루한컷 미션 중",
     buttonText: "공유하기",
   },
   IMAGE_CONFIRMED: {
-    title: "미션 업로드 성공",
-    subtitle: "소중한 오늘의 한컷을 기록했어요",
+    title: "하루한컷 미션 완료",
     buttonText: "보러가기",
   },
 } as const;
