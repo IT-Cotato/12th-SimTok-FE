7c5ded6daee6f7857cd04a3e733c04a89b2d294a
lemoncurdyogurt <suhosinsu1@naver.com>
2026-02-18 03:40:38 +0900

    refactor: 식물 친구 초대 로직 실제 친구목록으로 코드 수정

--- src/stores/useGardenStore.ts
+++ src/stores/useGardenStore.ts
@@ -1,14 +1,18 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
+import { FriendProfile } from "@/types/friendProfile.type";
+
 interface GardenState {
   selectedPlantId: string | null;
   nickname: string;
   invitedFriendId: number | null;
+  invitedFriend: FriendProfile | null; // 친구 객체 전체 저장 공간 추가
   message: string;
   setSelectedPlant: (id: string | null) => void;
   setNickname: (name: string) => void;
   setInvitedFriendId: (friendId: number | null) => void;
+  setInvitedFriend: (friend: FriendProfile | null) => void; // 설정 함수 추가
   resetForm: () => void;
   setMessage: (message: string) => void;
 }
@@ -19,17 +23,20 @@ export const useGardenStore = create<GardenState>()(
       selectedPlantId: null,
       nickname: "",
       invitedFriendId: null,
+      invitedFriend: null, // 초기값
       message: "",
 
       setSelectedPlant: id => set({ selectedPlantId: id }),
       setNickname: name => set({ nickname: name }),
       setInvitedFriendId: friendId => set({ invitedFriendId: friendId }),
+      setInvitedFriend: friend => set({ invitedFriend: friend }), // 구현
       setMessage: message => set({ message: message }),
       resetForm: () =>
         set({
           selectedPlantId: null,
           nickname: "",
           invitedFriendId: null,
+          invitedFriend: null,
           message: "",
         }),
     }),
