21089ebec6270290660b24cc845e434e26f955c9
lemoncurdyogurt <suhosinsu1@naver.com>
2026-02-18 14:23:39 +0900

    refactor: 내가 마지막으로 준 경우에 따라서 물주기버튼, 위의 제목 수정

--- src/utils/getGrowthImage.ts
+++ src/utils/getGrowthImage.ts
@@ -1,18 +1,24 @@
 import { PLANT_IMAGE_MAP } from "@/constants/garden/plantList";
+import { PLANT_SORT_INFO } from "@/constants/garden/plantList";
 import {
   PlantWaterStatus,
   PlantWaterStatusType,
 } from "@/constants/garden/plantStatus";
 
-import { GrowthStage } from "@/types/plant.type";
+import { GrowthStage, PlantSort } from "@/types/plant.type";
 
 export const getGrowthImage = (
   growthStatus: GrowthStage,
   plantStatus: PlantWaterStatusType,
+  plantName?: PlantSort,
 ) => {
-  if (growthStatus === "BLOOM") return "";
+  if (growthStatus === "BLOOM") {
+    const plantInfo = PLANT_SORT_INFO.find(item => item.id === plantName);
+    return plantInfo ? plantInfo.img : "";
+  }
 
   const imageSet = PLANT_IMAGE_MAP[growthStatus];
+  if (!imageSet) return "";
 
   return plantStatus === PlantWaterStatus.WITHERED
     ? imageSet.bad
