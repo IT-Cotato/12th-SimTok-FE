43145e8ec9d3b914293fa83e0b827d1d8c8cccbe
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-18 14:41:28 +0900

    refactor: 정원 홈 성장 완료 식물만 가져옴

--- src/components/garden/PlantColletction.tsx
+++ src/components/garden/PlantColletction.tsx
@@ -3,10 +3,10 @@
 import { PLANTS_PER_PAGE } from "@/constants/garden/gardenHome";
 import { PLANT_SORT_INFO } from "@/constants/garden/plantList";
 
-import { GardenPlant } from "@/types/plant.type";
+import { GardenPlantItem } from "@/types/plant.type";
 
 interface PlantColletctionProps {
-  plantList: GardenPlant[];
+  plantList: GardenPlantItem[];
   pageIndex: number;
 }
 
@@ -19,8 +19,8 @@ export const PlantColletction = ({
   const start = pageIndex * PLANTS_PER_PAGE;
   const plants = plantList.slice(start, start + PLANTS_PER_PAGE);
 
-  let frontPlants: GardenPlant[] = [];
-  let backPlants: GardenPlant[] = [];
+  let frontPlants: GardenPlantItem[] = [];
+  let backPlants: GardenPlantItem[] = [];
 
   if (plants.length <= FRONT_MAX) {
     frontPlants = plants;
@@ -46,13 +46,13 @@ export const PlantColletction = ({
         >
           {frontPlants.map(plant => {
             const plantImg =
-              PLANT_SORT_INFO.find(p => p.id === plant.plantSort)?.img ?? "";
+              PLANT_SORT_INFO.find(p => p.id === plant.plantName)?.img ?? "";
             return (
-              <div key={plant.id} className="flex justify-center">
+              <div key={plant.sharedPlantId} className="flex justify-center">
                 {plantImg && (
                   <img
                     src={plantImg}
-                    alt={plant.plantSort}
+                    alt={plant.plantName}
                     className="h-[115px] object-contain"
                   />
                 )}
@@ -66,13 +66,13 @@ export const PlantColletction = ({
           <div className="grid w-full max-w-lg grid-cols-3 justify-items-center gap-4 md:gap-6">
             {backPlants.map(plant => {
               const plantImg =
-                PLANT_SORT_INFO.find(p => p.id === plant.plantSort)?.img ?? "";
+                PLANT_SORT_INFO.find(p => p.id === plant.plantName)?.img ?? "";
               return (
-                <div key={plant.id} className="flex justify-center">
+                <div key={plant.sharedPlantId} className="flex justify-center">
                   {plantImg && (
                     <img
                       src={plantImg}
-                      alt={plant.plantSort}
+                      alt={plant.plantName}
                       className="h-[100px] object-contain opacity-90 md:h-[105px]"
                     />
                   )}
