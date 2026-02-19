d518c6117a26b0aa4df90a4632539f75515074a2
lemoncurdyogurt <suhosinsu1@naver.com>
2026-02-19 15:52:53 +0900

    feat: 내프로필 데이터 가져와서 초대코드 띄우기

--- src/app/friends/invite/page.tsx
+++ src/app/friends/invite/page.tsx
@@ -3,10 +3,14 @@ import { useRouter } from "next/navigation";
 
 import { useEffect, useState } from "react";
 
+import { getMyProfile } from "@/app/api/profile/profile.api";
+
 import { FullButton } from "@/components/common/FullButton";
 import { GlassStyleHeader } from "@/components/common/GlassStyleHeader";
 import { InputField } from "@/components/friends/InputField";
 import { QRCodeWrapper } from "@/components/friends/QRCodeWrapper";
+
+import { MyProfile } from "@/types/myProfile.type";
 
 const FriendInvitePage = () => {
   const router = useRouter();
@@ -14,14 +18,28 @@ const FriendInvitePage = () => {
   const [selectTitle, setSelectTitle] = useState<"left" | "right">("left");
   const [textCodeMode, setTextCodeMode] = useState(false);
   const [inputCode, setInputCode] = useState("");
+  const [myData, setMyData] = useState<MyProfile>();
 
   useEffect(() => {
     setTextCodeMode(selectTitle === "right");
   }, [selectTitle]);
 
+  useEffect(() => {
+    const fetchMyData = async () => {
+      try {
+        const data = await getMyProfile();
+        setMyData(data);
+        console.log("내프로필", data);
+      } catch (error) {
+        console.error("Failed to fetch profile:", error);
+      }
+    };
+    fetchMyData();
+  }, []);
+
   return (
     <main
-      className={`${selectTitle === "left" ? "bg-radial-mint" : inputCode.length > 0 ? "bg-radial-yellowgreen-mintgreen" : "bg-white"} flex h-full w-full flex-col`}
+      className={`${selectTitle === "left" ? "bg-radial-mint" : inputCode.length > 0 ? "bg-radial-yellowgreen-mintgreen" : "bg-white"} flex min-h-screen w-full flex-col`}
     >
       <GlassStyleHeader
         backHeader={true}
@@ -34,15 +52,17 @@ const FriendInvitePage = () => {
       {selectTitle === "left" && (
         <section className="flex flex-1 flex-col">
           {/* 상단: 남은 영역 채우기 */}
-          <div className="flex flex-1 items-center justify-center px-4 py-[10px]">
+          <div className="mt-[54.5px] flex items-center justify-center px-4">
             <sub className="text-sub-number text-neutral-01 text-center">
               QR코드를 친구에게 보여주세요!
             </sub>
           </div>
 
           {/* 하단: QR 코드 고정 */}
-          <div className="px-4 pb-[127.73px]">
-            <QRCodeWrapper textCodeMode={textCodeMode} />
+          <div className="flex flex-1 items-center px-4">
+            {myData && (
+              <QRCodeWrapper textCodeMode={textCodeMode} myData={myData} />
+            )}
           </div>
         </section>
       )}
@@ -50,11 +70,15 @@ const FriendInvitePage = () => {
       {selectTitle === "right" && (
         <>
           <section className="flex flex-1 flex-col gap-[30px] pb-[120px]">
-            <div className="px-4 py-[10px]">
-              <InputField onChangeInputText={setInputCode} />
-            </div>
-            <div className="relative px-4">
-              <QRCodeWrapper textCodeMode={textCodeMode} />
+            <div className="flex flex-1 flex-col">
+              <div className="px-4 py-[10px]">
+                <InputField onChangeInputText={setInputCode} />
+              </div>
+              <div className="relative flex flex-1 items-center px-4">
+                {myData && (
+                  <QRCodeWrapper textCodeMode={textCodeMode} myData={myData} />
+                )}
+              </div>
             </div>
           </section>
           <div className="fixed bottom-0 left-1/2 z-50 w-full max-w-[440px] -translate-x-1/2 bg-white px-4 py-[10px] pb-[42px]">
