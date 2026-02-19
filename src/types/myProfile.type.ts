cd61fb7925ba4653fdde56b1fb774c14e85e4a48
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-02-19 15:40:47 +0900

    feat: 내프로필 실제 데이터 가져오기 API 추가

--- src/types/myProfile.type.ts
+++ src/types/myProfile.type.ts
@@ -1,7 +1,8 @@
 export type MyProfile = {
   memberId: number;
-  profileImageUrl?: string;
+  profileImageUrl: string | null;
   name: string;
   phoneNumber: string; //01012345678
   birthDate: string; //0000.00.00
+  inviteCode: string;
 };
