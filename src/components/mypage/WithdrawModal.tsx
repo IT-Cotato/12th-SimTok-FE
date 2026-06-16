b01adeffec6c055589e9b523be703cf3ba56655d
idid10 <ejwuud2@naver.com>
2026-06-16 20:18:34 +0900

    design: 회원탈퇴 안내 문구 수정

--- src/components/mypage/WithdrawModal.tsx
+++ src/components/mypage/WithdrawModal.tsx
@@ -29,13 +29,13 @@ export const WithdrawModal = ({
     <div className="fixed inset-0 z-[100] flex items-center justify-center bg-black/50 px-4">
       <div className="flex w-full max-w-[316px] flex-col overflow-hidden rounded-[20px] bg-white pt-[10px]">
         <div className="flex flex-col items-center gap-1 px-6 pb-4">
-          <h3 className="text-h3 text-neutral-01">회원탈퇴</h3>
+          <h3 className="text-h3 text-neutral-01">회원 탈퇴 안내</h3>
           <p className="text-sub1-r text-neutral-02 mt-2 text-center">
             정말 탈퇴하시겠습니까?
             <br />
-            탈퇴 시 작성하신 게시글 및 모든 정보가
+            탈퇴 시 작성하신 모든 데이터가
             <br />
-            삭제되며 복구할 수 없습니다.
+            영구 삭제되며 복구할 수 없습니다.
           </p>
           <p className="text-sub1-sb text-neutral-02 mt-4 w-full text-left">
             탈퇴 하시는 이유를 알려주세요
